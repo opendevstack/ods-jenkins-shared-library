83be4d37ab23f7574f1ae1bb9146266bf6b7d863
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2018-10-26 16:12:22 +0200

    Set SonarQube projectVersion if not set in properties
    Closes #29.

--- vars/stageScanForSonarqube.groovy
+++ vars/stageScanForSonarqube.groovy
@@ -2,17 +2,25 @@ def call(def context) {
   if (context.sonarQubeBranch == '*' || context.sonarQubeBranch == context.gitBranch) {
     stage('SonarQube Analysis') {
       withSonarQubeEnv('SonarServerConfig') {
+        // debug mode
         def debugMode = ""
         if (context.debug) {
           debugMode = "-X"
         }
+        // project version
+        def projectVersionParam = ""
+        def propertiesContent = readFile('sonar-project.properties')
+        if (!propertiesContent.contains('sonar.projectVersion=')) {
+          projectVersionParam = "-Dsonar.projectVersion=${context.gitCommit.take(8)}"
+        }
+        // scan
         def scannerBinary = "sonar-scanner"
         def status = sh(returnStatus: true, script: "which ${scannerBinary}")
         if (status != 0) {
           def scannerHome = tool 'SonarScanner'
           scannerBinary = "${scannerHome}/bin/sonar-scanner"
         }
-        sh "${scannerBinary} ${debugMode}"
+        sh "${scannerBinary} ${debugMode} ${projectVersionParam}"
       }
     }
   }
