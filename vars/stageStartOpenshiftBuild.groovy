6b80bcd59b2a5593efe96e6e06f7e5ee43bcf735
dupontre <rene.dupont@boehringer-ingelheim.com>
2019-02-15 14:06:45 +0100

    Add missing image labels
    Adds information from Git to image via labels.

--- vars/stageStartOpenshiftBuild.groovy
+++ vars/stageStartOpenshiftBuild.groovy
@@ -13,10 +13,23 @@ def call(def context) {
 }
 
 private void patchBuildConfig(def context) {
-  sh """oc patch bc ${context.componentId} --type=json --patch '[
-    {"op": "replace", "path": "/spec/source", "value":{"type":"Binary"}},
-    {"op": "replace", "path": "/spec/output/to/name", "value":"${context.componentId}:${context.tagversion}"}
-    ]' -n ${context.targetProject}"""
+  def patches = [
+      '{"op": "replace", "path": "/spec/source", "value": {"type":"Binary"}}',
+      """{"op": "replace", "path": "/spec/output/to/name", "value": "${context.componentId}:${context.tagversion}"}""",
+      """{"op": "replace", "path": "/spec/output/imageLabels", "value": [
+        {"name":"ods.build.source.repo.commit.author","value":"${context.gitCommitAuthor}"},
+        {"name":"ods.build.source.repo.url","value":"${context.gitUrl}"},
+        {"name":"ods.build.source.repo.commit","value":"${context.gitCommit}"},
+        {"name":"ods.build.source.repo.commit.msg","value":"${context.gitCommitMessage}"},
+        {"name":"ods.build.source.repo.commit.time","value":"${context.gitCommitTime}"},
+        {"name":"ods.build.source.repo.branch","value":"${context.gitBranch}"},
+        {"name":"ods.build.jenkins.job.url","value":"${context.buildUrl}"},
+        {"name":"ods.build.date","value":"${context.buildTime}"},
+        {"name":"ods.build.lib.version","value":"${context.odsSharedLibVersion}"}
+      ]}"""
+  ]
+
+  sh """oc patch bc ${context.componentId} --type=json --patch '[${patches.join(",")}]' -n ${context.targetProject}"""
 }
 
 return this
