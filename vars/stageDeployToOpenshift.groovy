d41862fb226bfc344fde93c5f9864796776575f5
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2019-05-10 15:17:24 +0200

    Deploy via Tailor
    A deployment is not just updating an image, it might also involve adding
    an environment variable, or changing constraints etc. Because of this, a
    deployment should be one atomic JSON patchset.
    
    When an "openshift" folder is present, we use the "tailor" binary in the
    slave-base to apply its templates. The behaviour is customizable by
    adding a "Tailorfile" (or a more specific, per-project version of it).
    
    The actual rollout is triggered explicitly, which also allows us to
    monitor the progress and fail the build if the rollout does not work.
    
    As a consequence of this, when adding an "openshift" folder, the
    automatic triggers are removed, and the built container image is not
    tagged with "latest" anymore - we simply deploy the tag we just applied.
    This assumes that the template consumes the TAGVERSION param and that it
    uses it correctly to set the image. The latter is not checked, but the
    former at least errors if the TAGVERSION param is not consumed.

--- vars/stageDeployToOpenshift.groovy
+++ vars/stageDeployToOpenshift.groovy
@@ -5,13 +5,34 @@ def call(def context) {
       return
     }
 
-    openshiftTag(
-      srcStream: context.componentId,
-      srcTag: context.tagversion,
-      destStream: context.componentId,
-      destTag: "latest",
-      namespace: context.targetProject
-    )
+    if (fileExists('openshift')) {
+      // Output version to make it easier to diagnose issues.
+      sh "tailor version"
+      // Ensure that no automatic triggers exist as we rollout manually later on.
+      sh "oc --namespace ${context.targetProject} set triggers dc/${context.componentId} --manual"
+      // Detect any <project>.env, Tailorfile.<project> or Tailorfile files.
+      def paramFile = ''
+      if (fileExists("openshift/${context.targetProject}.env")) {
+        paramFile = "--param-file ${context.targetProject}.env"
+      }
+      def tailorFlagsAndArgs = "--non-interactive --namespace ${context.targetProject} --selector app=${context.projectId}-${context.componentId} update --ignore-path bc:/spec/output/imageLabels --ignore-path bc:/spec/strategy/dockerStrategy/buildArgs ${paramFile} --param TAGVERSION=${context.tagversion}"
+      if (fileExists("openshift/Tailorfile.${context.targetProject}")) {
+        tailorFlagsAndArgs = "--file Tailorfile.${context.targetProject} " + tailorFlagsAndArgs
+      } else if (fileExists("openshift/Tailorfile")) {
+        tailorFlagsAndArgs = "--file Tailorfile " + tailorFlagsAndArgs
+      }
+      // Update config via Tailor and rollout deployment.
+      sh "cd openshift && tailor ${tailorFlagsAndArgs}"
+      sh "oc --namespace ${context.targetProject} rollout latest dc/${context.componentId} && oc --namespace ${context.targetProject} rollout status dc/${context.componentId} --watch=true"
+    } else {
+      openshiftTag(
+        srcStream: context.componentId,
+        srcTag: context.tagversion,
+        destStream: context.componentId,
+        destTag: "latest",
+        namespace: context.targetProject
+      )
+    }
   }
 }
 
