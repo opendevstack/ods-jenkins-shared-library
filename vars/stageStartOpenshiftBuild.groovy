0a92a34aeba3b8d29159f446e2452e9ab084bcaf
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2019-05-17 14:54:22 +0200

    add addtl labels

--- vars/stageStartOpenshiftBuild.groovy
+++ vars/stageStartOpenshiftBuild.groovy
@@ -1,4 +1,4 @@
-def call(def context, def buildArgs = [:]) {
+def call(def context, def buildArgs = [:], def imageLabels = [:]) {
   stage('Build Openshift Image') {
     if (!context.environment) {
       println("Skipping for empty environment ...")
@@ -12,23 +12,39 @@ def call(def context, def buildArgs = [:]) {
   }
 }
 
-private void patchBuildConfig(def context, def buildArgs) {
+private void patchBuildConfig(def context, def buildArgs, def imageLabels) {
+  def sanitizedGitCommitMessage = context.gitCommitMessage.replaceAll("[\r\n]+", " ").trim().replaceAll("[\"']+", "")
+  
+  // create the default ODS driven labels
+  def odsImageLabels = []
+	odsImageLabels.push("{\"name\":\"ods.build.source.repo.url\",\"value\":\"${context.gitUrl}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.source.repo.commit.sha\",\"value\":\"${context.gitCommit}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.source.repo.commit.msg\",\"value\":\"${context.sanitizedGitCommitMessage}\"}")
+    odsImageLabels.push("{\"name\":\"ods.build.source.repo.commit.author\",\"value\":\"${context.gitCommitAuthor}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.source.repo.commit.timestamp\",\"value\":\"${context.gitCommitTime}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.source.repo.branch",\"value\":\"${context.gitBranch}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.jenkins.job.url\",\"value\":\"${context.buildUrl}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.timestamp\",\"value\":\"${context.buildTime}\"}")
+	odsImageLabels.push("{\"name\":\"ods.build.lib.version\",\"value\":\"${context.odsSharedLibVersion}\"}")
+	
+  // add additional ones - prefixed with ext.
+  for (def key : imageLabels.keySet()) {
+    def val = imageLabels[key]
+    odsImageLabels.push("{\"name\": \"ext.${key}\", \"value\": \"${val}\"}")
+  }
+  
+  def releaseFile = new File('docker/release.json')
+    release.append(${odsImageLabels.join(",")}, "UTF-8")
+  
   def patches = [
       '{"op": "replace", "path": "/spec/source", "value": {"type":"Binary"}}',
       """{"op": "replace", "path": "/spec/output/to/name", "value": "${context.componentId}:${context.tagversion}"}""",
       """{"op": "replace", "path": "/spec/output/imageLabels", "value": [
-        {"name":"ods.build.source.repo.commit.author","value":"${context.gitCommitAuthor}"},
-        {"name":"ods.build.source.repo.url","value":"${context.gitUrl}"},
-        {"name":"ods.build.source.repo.commit","value":"${context.gitCommit}"},
-        {"name":"ods.build.source.repo.commit.msg","value":"${context.gitCommitMessage}"},
-        {"name":"ods.build.source.repo.commit.time","value":"${context.gitCommitTime}"},
-        {"name":"ods.build.source.repo.branch","value":"${context.gitBranch}"},
-        {"name":"ods.build.jenkins.job.url","value":"${context.buildUrl}"},
-        {"name":"ods.build.date","value":"${context.buildTime}"},
-        {"name":"ods.build.lib.version","value":"${context.odsSharedLibVersion}"}
+        ${odsImageLabels.join(",")}
       ]}"""
   ]
 
+  // add build args
   def buildArgsItems = []
   for (def key : buildArgs.keySet()) {
     def val = buildArgs[key]
