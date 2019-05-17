f5846554fb862ef880e1e21cbc880affec876d6e
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2019-05-17 15:33:38 +0200

    catch typo

--- vars/stageStartOpenshiftBuild.groovy
+++ vars/stageStartOpenshiftBuild.groovy
@@ -37,7 +37,7 @@ private void patchBuildConfig(def context, def buildArgs, def imageLabels) {
   try {
     def releaseFile = new File('docker/release.json')
     release.append(${odsImageLabels.join(",")}, "UTF-8")
-  } (catch org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException accessException) {
+  } catch (org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException accessException) {
     println "Cannot create release.json - please approve in jenkins script approval!"
   }
   
