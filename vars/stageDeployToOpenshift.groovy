388583641c85863dcf22583736e8bc30e41642d4
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2019-05-24 14:26:09 +0200

    Allow .env as param file and add more explanation

--- vars/stageDeployToOpenshift.groovy
+++ vars/stageDeployToOpenshift.groovy
@@ -6,26 +6,33 @@ def call(def context, def selector) {
     }
 
     if (fileExists('openshift')) {
-      // Output version to make it easier to diagnose issues.
+      // Output Tailor version to make it easier to diagnose issues.
       sh "tailor version"
       // Ensure that no automatic triggers exist as we rollout manually later on.
       sh "oc --namespace ${context.targetProject} set triggers dc/${context.componentId} --manual"
-      // Detect any <project>.env, Tailorfile.<project> or Tailorfile files.
+      // Detect any <project>.env, .env, Tailorfile.<project> or Tailorfile files.
       def paramFile = ''
       if (fileExists("openshift/${context.targetProject}.env")) {
         paramFile = "--param-file ${context.targetProject}.env"
+      } else if (fileExists("openshift/.env")) {
+        paramFile = "--param-file .env"
       }
+      // Ensure default selector to prevent accidentally targeting the whole project.
       if (!selector) {
         selector = "app=${context.projectId}-${context.componentId}"
       }
-      def tailorFlagsAndArgs = "--non-interactive --namespace ${context.targetProject} --selector ${selector} update --ignore-path bc:/spec/output/imageLabels --ignore-path bc:/spec/strategy/dockerStrategy/buildArgs ${paramFile} --param TAGVERSION=${context.tagversion}"
+      // Do not touch BuildConfig paths that have been touched in stageStartOpenshiftBuild.
+      def ignorePaths = '--ignore-path bc:/spec/output/imageLabels --ignore-path bc:/spec/strategy/dockerStrategy/buildArgs'
+      // Pass tagversion to set the correct container image.
+      def tailorFlagsAndArgs = "--non-interactive --namespace ${context.targetProject} --selector ${selector} update ${ignorePaths} ${paramFile} --param TAGVERSION=${context.tagversion}"
       if (fileExists("openshift/Tailorfile.${context.targetProject}")) {
         tailorFlagsAndArgs = "--file Tailorfile.${context.targetProject} " + tailorFlagsAndArgs
       } else if (fileExists("openshift/Tailorfile")) {
         tailorFlagsAndArgs = "--file Tailorfile " + tailorFlagsAndArgs
       }
-      // Update config via Tailor and rollout deployment.
+      // Update config via Tailor.
       sh "cd openshift && tailor ${tailorFlagsAndArgs}"
+      // Rollout latest deployment, and wait until rollout is complete to gather the status code.
       sh "oc --namespace ${context.targetProject} rollout latest dc/${context.componentId} && oc --namespace ${context.targetProject} rollout status dc/${context.componentId} --watch=true"
     } else {
       openshiftTag(
