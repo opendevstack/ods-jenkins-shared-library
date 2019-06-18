4771c55cc7dfdbf185fd8974e9957b46182a3c33
Tobias Unger <tobias.unger.ext@boehringer-ingelheim.com>
2019-06-18 18:33:42 +0200

    Update createDocument

--- vars/createDocument.groovy
+++ vars/createDocument.groovy
@@ -14,8 +14,7 @@ def call(data = null, reportType = 'InstallationReport', reportVersion = '1.0', 
     requestData.settings.importToItems = false
     def payload = JsonOutput.toJson(requestData)
     println payload
-    def docGenSvcUrl = 'http://${host}:${port}/document'
-    println docGenSvcUrl
+    def docGenSvcUrl = "http://${host}:${port}/document"
     def post = new URL(docGenSvcUrl).openConnection()
     post.setRequestMethod("POST")
     post.setDoOutput(true)
