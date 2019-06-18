e66b6e12358a91d9bcab901e31dae5461027498c
Tobias Unger <tobias.unger.ext@boehringer-ingelheim.com>
2019-06-18 18:19:18 +0200

    Update env variables

--- vars/createDocument.groovy
+++ vars/createDocument.groovy
@@ -2,8 +2,8 @@ import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 
 def call(data = null, reportType = 'InstallationReport', reportVersion = '1.0', importToItems = false) {
-    def host = "${env.DOC_GEN_HOST}" ?: 'localhost'
-    def port = "${env.DOC_GEN_PORT}" ?: '8082'
+    def host = "${env.DOC_GEN_HOST}" ?: 'docgen.pltf'
+    def port = "${env.DOC_GEN_PORT}" ?: '8181'
 
     def requestData = [:]
     requestData.metadata = [:]
@@ -14,7 +14,7 @@ def call(data = null, reportType = 'InstallationReport', reportVersion = '1.0', 
     requestData.settings.importToItems = false
     def payload = JsonOutput.toJson(requestData)
     println payload
-    def docGenSvcUrl = 'http://localhost:8082/document'
+    def docGenSvcUrl = 'http://${host}:${port}/document'
     def post = new URL(docGenSvcUrl).openConnection()
     post.setRequestMethod("POST")
     post.setDoOutput(true)
