3e913d6315d07d03c528931a8edc21dfd6c597f1
Tobias Unger <tobias.unger.ext@boehringer-ingelheim.com>
2019-06-19 13:21:49 +0200

    Add createDocumentation

--- vars/createDocumentation.groovy
+++ vars/createDocumentation.groovy
@@ -2,7 +2,7 @@ def call(stage, repos, projectMetadata) {
     repos.each { repo ->
         def docData = createDocument('data')
         def docUrl = uploadDocumentToNexus(docData , 'org.opendevstack.rm', repo.name)
-        def task = queryJiraTask(projectMetadata, 'key = "PLTF-10"')
+        def task = queryJiraTask(projectMetadata, "project  = \"${projectMetadata.services.jira.project.key}\" AND labels = VP")
         addCommentToJira(task.key, docUrl)
     }
 }
