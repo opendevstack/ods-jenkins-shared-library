0ae55d7ad8eb5009be96ee5380a60c7a58b83714
Tobias Unger <tobias.unger.ext@boehringer-ingelheim.com>
2019-06-21 13:30:34 +0200

    Update runPipelinePhase

--- vars/runPipelinePhase.groovy
+++ vars/runPipelinePhase.groovy
@@ -1,26 +1,12 @@
-import org.ods.build.Steps
-
-def call(name, repos, buildScript = null, projectMetadata = null) {
+def call(name, repos) {
   repos.each { repo ->
     phaseConfig = repo.pipelineConfig.phases ? repo.pipelineConfig.phases[name] : null
     if (phaseConfig) {
       def label = "${repo.name} (${repo.url})"
-      def evaluationContext = [:]
-      if (projectMetadata) {
-        evaluationContext.projectMetadata = projectMetadata
-      }
-      if (buildScript) {
-        evaluationContext.params = script.params
-      }
+
       if (phaseConfig.type == 'Makefile') {
-        def task
-        if (!evaluationContext.isEmpty()) {
-          task = Steps.evaluateArgument(phaseConfig.task, evaluationContext)
-        } else {
-          task = phaseConfig.task
-        }
         dir("${WORKSPACE}/.tmp/${repo.name}") {
-          def script = "make ${task}"
+          def script = "make ${phaseConfig.task}"
           sh script: script, label: label
         }
       } else if (phaseConfig.type == 'ShellScript') {
@@ -28,17 +14,6 @@ def call(name, repos, buildScript = null, projectMetadata = null) {
           def script = "./scripts/${phaseConfig.script}"
           sh script: script, label: label
         }
-      }
-    } else if (phaseConfig.type == 'maven') {
-      def task
-      if (!evaluationContext.isEmpty()) {
-        task = Steps.evaluateArgument(phaseConfig.task, evaluationContext)
-      } else {
-        task = phaseConfig.task
-      }
-      dir("${WORKSPACE}/.tmp/${repo.name}") {
-        def script = "mvn ${task}"
-        sh script: script, label: label
       }
     } else {
       // Ignore undefined phases
