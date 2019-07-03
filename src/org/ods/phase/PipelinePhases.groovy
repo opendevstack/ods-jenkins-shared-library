922d30fd816374282d13f9b2a0abe7af4840e383
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2019-07-03 21:00:05 +0200

    Rename pipeline phase names.

--- src/org/ods/phase/PipelinePhases.groovy
+++ src/org/ods/phase/PipelinePhases.groovy
@@ -1,10 +1,10 @@
 package org.ods.phase
 
 class PipelinePhases {
-    static final String BUILD_PHASE   = 'build'
-    static final String DEPLOY_PHASE  = 'deploy'
-    static final String TEST_PHASE    = 'test'
-    static final String RELEASE_PHASE = 'release'
+    static final String BUILD_PHASE   = "Build"
+    static final String DEPLOY_PHASE  = "Deploy"
+    static final String TEST_PHASE    = "Test"
+    static final String RELEASE_PHASE = "Release"
 
     static final List ALWAYS_PARALLEL_PHASES = [ BUILD_PHASE ]
 }
