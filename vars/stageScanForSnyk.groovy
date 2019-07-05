16608c51739a53f309f0b1825fc0170b1c07adf4
Titakis,Sebastian (BI X) BIX-DE-I <sebastian.titakis@boehringer-ingelheim.com>
2019-07-05 13:31:00 +0200

    adds organisation param

--- vars/stageScanForSnyk.groovy
+++ vars/stageScanForSnyk.groovy
@@ -1,7 +1,10 @@
-def call(def context, def snykAuthenticationCode, def buildFile) {
+def call(def context, def snykAuthenticationCode, def buildFile, def organisation) {
   if (null == snykAuthenticationCode) {
     println("Skipping Snyk Scan due to missing authentication code (to enable pass as parameter a snyk service account authentication code to this stage)")
     return
+  }
+  if (null == organisation) {
+    error "Build failed: organisation is null!"
   }
 
   String message = "Snyk scan mode: build will " + (context.doFailOnSnykScanVulnerabilities ? "" : "not ") +
@@ -13,24 +16,24 @@ def call(def context, def snykAuthenticationCode, def buildFile) {
   } else {
     stage('Snyk Security Scan') {
       withEnv(["SNYK_AUTHENTICATION_CODE=${snykAuthenticationCode}", "PROJECT_NAME=${context.targetProject}",
-               "COMPONENT_NAME=${context.componentId}", "BUILD_FILE=${buildFile}"]) {
+               "COMPONENT_NAME=${context.componentId}", "BUILD_FILE=${buildFile}", "ORGANISATION=${organisation}"]) {
         // Verify that snyk is installed
-        def status = sh(script: "/usr/local/snyk version", returnStatus: true)
+        def status = sh(script: "snyk version", returnStatus: true)
         if (status != 0) {
           error "Build failed: snyk is not installed!"
         }
         // Authorise snyk
-        status = sh(script: "/usr/local/snyk auth $SNYK_AUTHENTICATION_CODE", returnStatus: true)
+        status = sh(script: "snyk auth $SNYK_AUTHENTICATION_CODE", returnStatus: true)
         if (status != 0) {
           error "Build failed: something went wrong by authorising snyk (SNYK_AUTHENTICATION_CODE=$SNYK_AUTHENTICATION_CODE)!"
         }
         // first monitor project
-        status = sh(script: "/usr/local/snyk monitor --file=$BUILD_FILE --project-name=$PROJECT_NAME/$COMPONENT_NAME", returnStatus: true)
+        status = sh(script: "snyk monitor -org=$ORGANISATION --file=$BUILD_FILE --project-name=$PROJECT_NAME/$COMPONENT_NAME", returnStatus: true)
         if (status != 0) {
           error "Build failed: something went wrong with snyk monitor command!"
         }
         // fail if vulnerabilites are found
-        status = sh(script: "/usr/local/snyk test --file=$BUILD_FILE", returnStatus: true)
+        status = sh(script: "snyk test -org=$ORGANISATION --file=$BUILD_FILE", returnStatus: true)
         if (status != 0 && context.doFailOnSnykScanVulnerabilities) {
           error "Build failed: snyk test found vulnerabilities (see snyk report above for details!)!"
         }
