694652808dac51784e15a983378a98f4a33c88b4
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2019-07-16 14:05:23 +0200

    Improve test phase implementation. Use constants in PipelinePhases class.

--- vars/phaseDeploy.groovy
+++ vars/phaseDeploy.groovy
@@ -1,8 +1,10 @@
+import org.ods.phase.PipelinePhases
 import org.ods.util.MultiRepoOrchestrationPipelineUtil
 
 def call(Map metadata, List<Set<Map>> repos) {
+    // Execute phase for each repository
     def util = new MultiRepoOrchestrationPipelineUtil(this)
-    util.prepareExecutePhaseForReposNamedJob('Deploy', repos)
+    util.prepareExecutePhaseForReposNamedJob(PipelinePhases.DEPLOY_PHASE, repos)
         .each { group ->
             parallel(group)
         }
--- vars/phaseTest.groovy
+++ vars/phaseTest.groovy
@@ -1,8 +1,14 @@
+import org.ods.phase.PipelinePhases
 import org.ods.util.MultiRepoOrchestrationPipelineUtil
 
 def call(Map metadata, List<Set<Map>> repos) {
+    // Get a list of automated test scenarios from Jira
+    def issues = jiraGetIssuesForJQLQuery(metadata, "project = ${metadata.id} AND labels = AutomatedTest AND issuetype = sub-task")
+        .collect { [ id: it.id, key: it.key, summary: it.fields.summary, description: it.fields.description, url: it.self ]}
+
+    // Execute phase for each repository
     def util = new MultiRepoOrchestrationPipelineUtil(this)
-    util.prepareExecutePhaseForReposNamedJob('Test', repos)
+    util.prepareExecutePhaseForReposNamedJob(PipelinePhases.TEST_PHASE, repos)
         .each { group ->
             parallel(group)
         }
