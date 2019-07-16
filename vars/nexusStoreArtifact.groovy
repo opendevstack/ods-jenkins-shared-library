22a886d02e117ef34daa84b5484281bb8e58f89c
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2019-07-16 09:08:06 +0200

    Clean code and documentation.

--- vars/nexusStoreArtifact.groovy
+++ vars/nexusStoreArtifact.groovy
@@ -1,11 +1,10 @@
 import org.ods.service.NexusService
 
-// Store a document in Nexus and return a URI to the document
+// Store a document in Nexus and return a URL to the document
 def call(Map metadata, String repository, String directory, String id, String version, byte[] document, String contentType) {
-    def result = new NexusService(env.NEXUS_URL, env.NEXUS_USERNAME, env.NEXUS_PASSWORD)
+    return new NexusService(env.NEXUS_URL, env.NEXUS_USERNAME, env.NEXUS_PASSWORD)
         .storeArtifact(repository, directory, "${id}-${version}.pdf", document, contentType)
-
-    return result.toString()
+        .toString()
 }
 
 return this
