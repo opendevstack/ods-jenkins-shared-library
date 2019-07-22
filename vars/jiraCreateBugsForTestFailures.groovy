b07a4792c4b51e14b1e01e3997e8b1b0a0f76f73
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2019-07-22 16:17:54 +0200

    Use Jenkins credentials instead of hard-coded username and password.

--- vars/jiraCreateBugsForTestFailures.groovy
+++ vars/jiraCreateBugsForTestFailures.groovy
@@ -2,16 +2,15 @@ import org.ods.service.JiraService
 
 // Create Jira bugs for test failures
 def call(Map metadata, Collection testFailures, Map jiraIssues) {
-//    withCredentials([ usernamePassword(credentialsId: metadata.services.jira.credentials.id, usernameVariable: "JIRA_USERNAME", passwordVariable: "JIRA_PASSWORD") ]) {
-//        def jira = new JiraService(env.JIRA_URL, env.JIRA_USERNAME, env.JIRA_PASSWORD)
-        def jira = new JiraService(env.JIRA_URL, "admin", "admin")
+    withCredentials([ usernamePassword(credentialsId: metadata.services.jira.credentials.id, usernameVariable: "JIRA_USERNAME", passwordVariable: "JIRA_PASSWORD") ]) {
+        def jira = new JiraService(env.JIRA_URL, env.JIRA_USERNAME, env.JIRA_PASSWORD)
 
         // TODO: also handle errors
         testFailures.each { failure ->
             createBugAndLinkTestCases(metadata, jira, failure, jiraIssues)
         }
     }
-//}
+}
 
 private def createBugAndLinkTestCases(def metadata, def jira, def failure, def jiraIssues) {
     // Create a Jira bug for the failure in the current project
