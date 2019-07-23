34efef2db27b51b06dc9e830ac3dbad5cb4f72c6
MTI <MTI@NLWHG2218.opitz-consulting.int>
2019-07-23 10:02:05 +0200

    removed commented out code

--- vars/nexusStoreZipArtifact.groovy
+++ vars/nexusStoreZipArtifact.groovy
@@ -1,29 +1,12 @@
 import org.ods.service.NexusService
 
-// Store a document in Nexus and return a URL to the document
-def call(Map metadata, String repository, String directory, String id, String version, String jenkinsDirectory) {
-	
-	
-	def testfile = zip(zipFile: jenkinsDirectory + "RawData.zip", dir: jenkinsDirectory)
-	
-//	def zipFileName = "RawData.zip"
-//	
-//			ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream(zipFileName))
-//			files.each { file ->
-//				//check if file
-//				if (file.getClass().equals(File.class)){
-//					zipFile.putNextEntry(new ZipEntry(file.name))
-//					def buffer = new byte[file.size()]
-//					file.withInputStream {
-//						zipFile.write(buffer, 0, it.read(buffer))
-//					}
-//					zipFile.closeEntry()
-//				}
-//			}
-//			zipFile.close()
+// Store a zip archive in Nexus and return a URL to the archive
+def call(Map metadata, String repository, String directory, String id, String version, String jenkinsWorkSpaceDirectory) {
+		
+	zip(zipFile: jenkinsWorkSpaceDirectory + "RawData.zip", dir: jenkinsWorkSpaceDirectory)
 	
     return new NexusService(env.NEXUS_URL, env.NEXUS_USERNAME, env.NEXUS_PASSWORD)
-        .storeArtifact(repository, directory, "${id}-${version}.zip", new File(jenkinsDirectory+"RawData.zip").getBytes(), "document/zip")
+        .storeArtifact(repository, directory, "${id}-${version}.zip", new File(jenkinsWorkSpaceDirectory+"RawData.zip").getBytes(), "document/zip")
         .toString()
 }
 
