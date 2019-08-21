7c3d2abc3de6f4750e5c50446d1c9fac24d9ad97
Gerard Castillo <gerard.castillo@boehringer-ingelheim.com>
2019-08-21 18:39:32 +0200

    arroba to ASCII for clone project script only

--- src/org/ods/OdsPipeline.groovy
+++ src/org/ods/OdsPipeline.groovy
@@ -160,13 +160,14 @@ class OdsPipeline implements Serializable {
 
       logger.info 'Environment does not exist yet. Creating now ...'
       script.withCredentials([script.usernameColonPassword(credentialsId: context.credentialsId, variable: 'USERPASS')]) {
-        def userPass = script.USERPASS.replace('@', '%40').replace('$', '\'$\'')
+        def userPass = script.USERPASS.replace('$', '\'$\'')
         def cloneProjectScriptUrl = "https://${context.bitbucketHost}/projects/opendevstack/repos/ods-project-quickstarters/raw/ocp-templates/scripts/clone-project.sh?at=refs%2Fheads%2Fproduction"
         script.sh(script: "curl --fail -s --user ${userPass} -G '${cloneProjectScriptUrl}' -d raw -o clone-project.sh")
         def debugMode = ""
         if (context.debug) {
           debugMode = "--debug"
         }
+        userPass = userPass.replace('@', '\\@')
         script.sh(script: "sh clone-project.sh -o ${context.openshiftHost} -b ${context.bitbucketHost} -c ${userPass} -p ${context.projectId} -s ${context.cloneSourceEnv} -t ${context.environment} ${debugMode}")
         logger.info 'Environment created!'
       }
