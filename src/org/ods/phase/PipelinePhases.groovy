0f544b91184c8618e2f5a3b8ddc435d9c28bb10c
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2019-09-08 12:47:07 +0200

    Add 'Finalize' pipeline phase stub.

--- src/org/ods/phase/PipelinePhases.groovy
+++ src/org/ods/phase/PipelinePhases.groovy
@@ -1,10 +1,11 @@
 package org.ods.phase
 
 class PipelinePhases {
-    static final String BUILD_PHASE   = "Build"
-    static final String DEPLOY_PHASE  = "Deploy"
-    static final String TEST_PHASE    = "Test"
-    static final String RELEASE_PHASE = "Release"
+    static final String BUILD_PHASE    = "Build"
+    static final String DEPLOY_PHASE   = "Deploy"
+    static final String FINALIZE_PHASE = "Finalize"
+    static final String RELEASE_PHASE  = "Release"
+    static final String TEST_PHASE     = "Test"
 
     static final List ALWAYS_PARALLEL_PHASES = []
 }
