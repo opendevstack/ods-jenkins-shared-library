ef1c2f2b6ddd04fef05123392e4f41067e5f4d2b
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2019-10-14 08:53:04 +0200

    Bring back environment name in the label

--- src/org/ods/OpenshiftUtils.groovy
+++ src/org/ods/OpenshiftUtils.groovy
@@ -5,7 +5,7 @@ class OpenshiftUtils {
     static boolean environmentExists(def script, String name) {
         def statusCode = script.sh(
             script:"oc project ${name} &> /dev/null",
-            label: "check if OCP environment exists",
+            label: "check if OCP environment ${name} exists",
             returnStatus: true
         )
         return statusCode == 0
