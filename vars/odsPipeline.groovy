c21f8bfac455bae661d71041014a420e47f2f65c
Utschig-Utschig,Clemens (IT) BIG-AT-V <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-03-18 09:21:57 +0000

    remove start stage

--- vars/odsPipeline.groovy
+++ vars/odsPipeline.groovy
@@ -3,17 +3,13 @@ import org.ods.OdsPipeline
 
 def call(Map config, Closure body) {
 
-  OdsPipeline pipeline
-
-  stage('odsPipeline start') {
-    def debug = env.DEBUG
-    if (debug != null && config.debug == null) {
-      config.debug = debug
-    }
-
-    def logger = new OdsLogger(this, debug)
-    pipeline = new OdsPipeline(this, logger)
+  def debug = env.DEBUG
+  if (debug != null && config.debug == null) {
+    config.debug = debug
   }
+
+  def logger = new OdsLogger(this, debug)
+  OdsPipeline pipeline = new OdsPipeline(this, logger)
 
   pipeline.execute(config, body)
 }
