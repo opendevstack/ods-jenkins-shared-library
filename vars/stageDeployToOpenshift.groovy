12bf095b27501add62afd32a3ecae4094c998fcf
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-04-13 18:32:19 +0200

    remove return from legacy stages - so they do NOT provide this functionality

--- vars/stageDeployToOpenshift.groovy
+++ vars/stageDeployToOpenshift.groovy
@@ -1,6 +1,6 @@
 def call(def context, Map config = [:]) {
   echo "'stageDeployToOpenshift' has been replaced with 'odsComponentStageRolloutOpenShiftDeployment', please use that instead."
-  return odsComponentStageRolloutOpenShiftDeployment(context, config)
+  odsComponentStageRolloutOpenShiftDeployment(context, config)
 }
 
 return this
--- vars/stageStartOpenshiftBuild.groovy
+++ vars/stageStartOpenshiftBuild.groovy
@@ -2,7 +2,7 @@ import org.ods.component.IContext
 
 def call(IContext context, Map buildArgs = [:], Map imageLabels = [:]) {
   echo "'stageStartOpenShiftbuild' has been replaced with 'odsComponentStageBuildOpenShiftImage', please use that instead."
-  return odsComponentStageBuildOpenShiftImage(
+  odsComponentStageBuildOpenShiftImage(
     context,
     [buildArgs: buildArgs, imageLabels: imageLabels]
   )
