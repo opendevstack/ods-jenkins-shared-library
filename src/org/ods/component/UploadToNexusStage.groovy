cb8b81b30024258254a413c093dfe661e1b5bc0c
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-04-27 13:06:21 +0200

    put back to run

--- src/org/ods/component/UploadToNexusStage.groovy
+++ src/org/ods/component/UploadToNexusStage.groovy
@@ -3,18 +3,15 @@ package org.ods.component
 class UploadToNexusStage extends Stage {
   public final String STAGE_NAME = 'Upload to Nexus'
 
-  def repoType
-  def distFile
-  def groupId
-  
   UploadToNexusStage(def script, IContext context, Map config) {
     super(script, context, config)
-    repoType = config.repoType ?: "candidates"
-    distFile = config.distributionFile ?: "${componentId}-${context.tagversion}.tar.gz"
-    groupId  = config.groupId ?: context.groupId
   }
 
   def run() {
+    def repoType = config.repoType ?: "candidates"
+    def distFile = config.distributionFile ?: "${componentId}-${context.tagversion}.tar.gz"
+    def groupId  = config.groupId ?: context.groupId
+
     def uploadPath = "${context.nexusHost}/repository/${repoType}/${groupId.replace('.', '/')}/${componentId}/${context.tagversion}/${distFile}"
     script.echo ("Uploading ${distFile} to ${uploadPath}")
     script.sh (
