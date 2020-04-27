ad2fe4d866be2111ad70ec735b77016610e7980d
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-04-27 13:46:32 +0200

    bring back again

--- src/org/ods/component/UploadToNexusStage.groovy
+++ src/org/ods/component/UploadToNexusStage.groovy
@@ -3,23 +3,26 @@ package org.ods.component
 class UploadToNexusStage extends Stage {
   public final String STAGE_NAME = 'Upload to Nexus'
 
+  def repoType
+  def distFile
+  def groupId
+  def uploadPath
+
   UploadToNexusStage(def script, IContext context, Map config) {
     super(script, context, config)
+    repoType = config.repoType ?: "candidates"
+    distFile = config.distributionFile ?: "${componentId}-${context.tagversion}.tar.gz"
+    groupId  = config.groupId ?: context.groupId
+    uploadPath = "${context.nexusHost}/repository/${repoType}/${groupId.replace('.', '/')}/${componentId}/${context.tagversion}/${distFile}"
   }
 
   def run() {
-    /*
-    String repoType = config.repoType ?: "candidates"
-    String distFile = config.distributionFile ?: "${componentId}-${context.tagversion}.tar.gz"
-    String groupId  = config.groupId ?: context.groupId
-    String uploadPath = "${context.nexusHost}/repository/${repoType}/${groupId.replace('.', '/')}/${componentId}/${context.tagversion}/${distFile}"
-    
     script.echo ("Uploading ${distFile} to ${uploadPath}")
     script.sh (
       script : "curl -u ${context.nexusUsername}:${context.nexusPassword} --upload-file ${distFile} ${uploadPath}",
       label : "Uploading ${distFile} to Nexus"
     )
     
-    return uploadPath */
+    return uploadPath
   }
 }
