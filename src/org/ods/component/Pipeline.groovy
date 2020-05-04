opendevstack/ods-jenkins-shared-library

commit 949f241859d49efca6ea1369e3f50f40e6b17306

Feature/add mono repo support (#242)

src/org/ods/component/Pipeline.groovy
@@ -5,6 +5,7 @@ import org.ods.services.BitbucketService
 import org.ods.services.OpenShiftService
 import org.ods.services.SonarQubeService
 import org.ods.services.ServiceRegistry
+import groovy.json.JsonOutput
 
 class Pipeline implements Serializable {
 
@@ -51,12 +52,6 @@ class Pipeline implements Serializable {
             ))
 
             registry.add(OpenShiftService, new OpenShiftService(script, context.targetProject))
-            openShiftService = registry.get(OpenShiftService)
-          }
-
-          def autoCloneEnabled = !!context.cloneSourceEnv
-          if (autoCloneEnabled) {
-            createOpenShiftEnvironment(context)
           }
 
           skipCi = isCiSkip()
@@ -64,6 +59,14 @@ class Pipeline implements Serializable {
             logger.info 'Skipping build due to [ci skip] in the commit message ...'
             updateBuildStatus('NOT_BUILT')
             setBitbucketBuildStatus('SUCCESSFUL')
+          } else {
+            context.setOpenshiftApplicationDomain (
+              ServiceRegistry.instance.get(OpenShiftService).getOpenshiftApplicationDomain())
+  
+            def autoCloneEnabled = !!context.cloneSourceEnv
+            if (autoCloneEnabled) {
+              createOpenShiftEnvironment(context)
+            }
           }
         }
       } catch (err) {
@@ -143,6 +146,8 @@ class Pipeline implements Serializable {
                 throw err
               }
             }
+          } finally {
+            logger.debug ("ODS Build Artifacts: \r${JsonOutput.prettyPrint(JsonOutput.toJson(context.getBuildArtifactURIs()))}")
           }
         }
       }
@@ -323,7 +328,12 @@ class Pipeline implements Serializable {
 
   def updateBuildStatus(String status) {
     if (context.displayNameUpdateEnabled) {
-      script.currentBuild.result = status
+      // @ FIXME ? groovy.lang.MissingPropertyException: No such property: result for class: java.lang.String
+      if (script.currentBuild instanceof String) {
+        script.currentBuild = status
+      } else {
+        script.currentBuild.result = status
+      }
     }
   }
 

src/org/ods/component/BuildOpenShiftImageStage.groovy
@@ -20,6 +20,10 @@ class BuildOpenShiftImageStage extends Stage {
     if (!config.buildTimeoutMinutes) {
       config.buildTimeoutMinutes = context.openshiftBuildTimeout
     }
+    if (!config.dockerDir) {
+      config.dockerDir = context.dockerDir
+    }
+    
     this.openShift = openShift
   }
 
@@ -30,7 +34,7 @@ class BuildOpenShiftImageStage extends Stage {
     }
 
     def imageLabels = buildImageLabels()
-    writeReleaseFile(imageLabels)
+    writeReleaseFile(imageLabels, config)
     patchBuildConfig(imageLabels)
 
     // Start and follow build of container image.
@@ -41,30 +45,31 @@ class BuildOpenShiftImageStage extends Stage {
     // Retrieve build status.
     def lastVersion = getLastVersion()
     if (!lastVersion) {
-      script.error "Could not get last version of BuildConfig '${context.componentId}'."
+      script.error "Could not get last version of BuildConfig '${componentId}'."
     }
-    def buildId = "${context.componentId}-${lastVersion}"
+    def buildId = "${componentId}-${lastVersion}"
     def buildStatus = getBuildStatus(buildId)
     if (buildStatus != 'complete') {
       script.error "OpenShift Build #${lastVersion} was not successful - status is '${buildStatus}'."
     }
-    context.addArtifactURI("OCP Build Id", buildId)
-
     def imageReference = getImageReference()
     script.echo "Build #${lastVersion} has produced image: ${imageReference}."
-    context.addArtifactURI("OCP Docker image", imageReference)
+    
+    context.addBuildToArtifactURIs (componentId, [ "buildId" : buildId, "image" : imageReference ])
+    
+    return ["buildId" : buildId, "image" : imageReference]
   }
 
     private String getImageReference() {
-      openShift.getImageReference(context.componentId, context.tagversion)
+      openShift.getImageReference(componentId, context.tagversion)
     }
 
     private String startAndFollowBuild() {
-      openShift.startAndFollowBuild(context.componentId, context.dockerDir)
+      openShift.startAndFollowBuild(componentId, config.dockerDir)
     }
 
     private String getLastVersion() {
-      openShift.getLastBuildVersion(context.componentId)
+      openShift.getLastBuildVersion(componentId)
     }
 
     private String getBuildStatus(String build) {
@@ -73,7 +78,7 @@ class BuildOpenShiftImageStage extends Stage {
 
     private String patchBuildConfig(Map imageLabels) {
       openShift.patchBuildConfig(
-        context.componentId,
+        componentId,
         context.tagversion,
         config.buildArgs,
         imageLabels
@@ -106,13 +111,13 @@ class BuildOpenShiftImageStage extends Stage {
     sanitizedImageLabels
   }
 
-  private writeReleaseFile(Map imageLabels) {
+  private writeReleaseFile(Map imageLabels, Map config) {
     def jsonImageLabels = []
     for (def key : imageLabels.keySet()) {
       jsonImageLabels << """{"name": "${key}", "value": "${imageLabels[key]}"}"""
     }
 
     // Write docker/release.json file to be reachable from Dockerfile.
-    script.writeFile(file: 'docker/release.json', text: "[\n" + jsonImageLabels.join(",\n") + "\n]")
+    script.writeFile(file: '${config.dockerDir}/release.json', text: "[\n" + jsonImageLabels.join(",\n") + "\n]")
   }
 }

src/org/ods/component/Context.groovy
@@ -10,7 +10,7 @@ class Context implements IContext {
   private Map config
   private Logger logger
 
-  private def artifactUriStore = [:]
+  private def artifactUriStore = [ "builds" : [ : ], "deployments" : [ : ]]
 
   Context(def script, Map config, Logger logger) {
     this.script = script
@@ -483,6 +483,7 @@ class Context implements IContext {
     config.displayNameUpdateEnabled = displayNameUpdateEnabled
   }
 
+  @NonCPS
   String getDockerDir() {
     return config.dockerDir
   }
@@ -649,7 +650,7 @@ class Context implements IContext {
       return
     }
 
-    logger.info "No environment to deploy to was determined " +
+    logger.info "No environment to deploy to was determined, returning" +
         "[gitBranch=${config.gitBranch}, projectId=${config.projectId}]"
     config.environment = ""
     config.cloneSourceEnv = ""
@@ -700,12 +701,20 @@ class Context implements IContext {
     return m
   }
 
-  public Map<String, String> getBuildArtifactURIs() {
-    return this.artifactUriStore
+  public Map<String, Object> getBuildArtifactURIs() {
+    return artifactUriStore.asImmutable()
+  }
+  
+  public void addArtifactURI(String key, value) {
+    artifactUriStore.put(key, value)
   }
 
-  public void addArtifactURI(String key, value) {
-    this.artifactUriStore.put(key, value)
+  public void addBuildToArtifactURIs (String buildConfigName, Map <String, String> buildInformation) {
+    artifactUriStore.builds [buildConfigName] = buildInformation
+  }
+  
+  public void addDeploymentToArtifactURIs (String deploymentConfigName, Map deploymentInformation) {
+    artifactUriStore.deployments [deploymentConfigName] = deploymentInformation
   }
 
   // get extension image labels
@@ -738,4 +747,15 @@ class Context implements IContext {
         kvMap
       }
   }
+  
+  // set the application domain
+  void setOpenshiftApplicationDomain (String domain) {
+    config.domain = domain
+  }
+
+  // get the application domain
+  String getOpenshiftApplicationDomain () {
+    return config.domain
+  }
+
 }

src/org/ods/component/IContext.groovy
@@ -199,15 +199,28 @@ interface IContext {
     String getDockerDir()
 
     // get any build artifact URIs there were created
-    public Map<String, String> getBuildArtifactURIs()
+    public Map<String, Object> getBuildArtifactURIs()
 
     // adds a build artifact URI to the context for later retrieval,
     // e.g. in case a stage fails - the failed stage name - with key failedStage
     public void addArtifactURI (String key, value)
 
+    // add a build to the artifact urls (key buildConfig name)
+    public void addBuildToArtifactURIs (String buildConfigName, Map <String, String> buildInformation)
+    
+    // add a deployment to the artifact urls (key deploymentConfig name)
+    public void addDeploymentToArtifactURIs (String deploymentConfigName, Map deploymentInformation)
+    
     // get extension image labels
     public Map<String, String> getExtensionImageLabels ()
 
     // set and add image labels
     void setExtensionImageLabels (Map <String, String> extensionLabels)
+    
+    // set the application domain 
+    void setOpenshiftApplicationDomain (String domain)
+
+    // get the application domain
+    String getOpenshiftApplicationDomain ()
+    
 }

src/org/ods/component/RolloutOpenShiftDeploymentStage.groovy
@@ -5,7 +5,7 @@ import org.ods.services.OpenShiftService
 class RolloutOpenShiftDeploymentStage extends Stage {
   public final String STAGE_NAME = 'Deploy to Openshift'
   private OpenShiftService openShift
-
+  
   RolloutOpenShiftDeploymentStage(def script, IContext context, Map config, OpenShiftService openShift) {
     super(script, context, config)
     if (!config.deployTimeoutMinutes) {
@@ -22,12 +22,18 @@ class RolloutOpenShiftDeploymentStage extends Stage {
 
     def dcExists = deploymentConfigExists()
     if (!dcExists) {
-      script.error "DeploymentConfig '${context.componentId}' does not exist."
+      script.error "DeploymentConfig '${componentId}' does not exist."
     }
+
+    config.imagestreams = openShift.getImageStreamsForDeploymentConfig(componentId)
 
     def isExists = imageStreamExists()
     if (!isExists) {
-      script.error "ImageStream '${context.componentId}' does not exist."
+      List imageStreamNamesNice = []
+      config.imagestreams.each { imageStream ->
+        imageStreamNamesNice << "${imageStream.imageStreamProject}/${imageStream.imageStream}"
+      }
+      script.error "One of the imagestreams '${imageStreamNamesNice}' for component '${componentId}' does not exist."
     }
 
     def imageTriggerEnabled = automaticImageChangeTriggerEnabled()
@@ -43,47 +49,69 @@ class RolloutOpenShiftDeploymentStage extends Stage {
 
     def latestVersion = getLatestVersion()
     if (!latestVersion) {
-      script.error "Could not get latest version of DeploymentConfig '${context.componentId}'."
+      script.error "Could not get latest version of DeploymentConfig '${componentId}'."
     }
-    def replicationController = "${context.componentId}-${latestVersion}"
+    def replicationController = "${componentId}-${latestVersion}"
     def rolloutStatus = getRolloutStatus(replicationController)
     if (rolloutStatus != "complete") {
       script.error "Deployment #${latestVersion} failed with status '${rolloutStatus}', please check the error in the OpenShift web console."
     } else {
       script.echo "Deployment #${latestVersion} successfully rolled out."
-      context.addArtifactURI("OCP Deployment Id", replicationController)
     }
+    def pod = getPodDataForRollout(replicationController)
+    script.echo "Pod ${pod} for #${latestVersion}"
+    context.addDeploymentToArtifactURIs (componentId, pod)
+    
+    return pod
   }
 
   private boolean deploymentConfigExists() {
-    openShift.resourceExists('DeploymentConfig', context.componentId)
+    openShift.resourceExists('DeploymentConfig', componentId)
   }
 
   private boolean imageStreamExists() {
-    openShift.resourceExists('ImageStream', context.componentId)
+    boolean allStreamExists = true
+    config.imagestreams.each { imageStream ->
+      script.echo ("Checking imagestream ${imageStream} against ${context.targetProject}")
+      if (imageStream.imageStreamProject == context.targetProject &&
+          !openShift.resourceExists('ImageStream', imageStream.imageStream)) {
+          allStreamExists = false
+      }
+    }
+    return allStreamExists
   }
 
   private boolean automaticImageChangeTriggerEnabled() {
-    openShift.automaticImageChangeTriggerEnabled(context.componentId)
+    openShift.automaticImageChangeTriggerEnabled(componentId)
   }
 
   private void setImageTagLatest() {
-    openShift.setImageTag(context.componentId, context.tagversion, 'latest')
+    config.imagestreams.each { imageStream ->
+      // only tag imagestreams that this project owns
+      if (context.targetProject == imageStream.imageStreamProject)
+      {
+        openShift.setImageTag(imageStream.imageStream, context.tagversion, 'latest')
+      }  
+    }
   }
 
   private void startRollout() {
-    openShift.startRollout(context.componentId)
+    openShift.startRollout(componentId)
   }
 
   private void watchRollout() {
-    openShift.watchRollout(context.componentId)
+    openShift.watchRollout(componentId)
   }
 
   private String getLatestVersion() {
-    openShift.getLatestVersion(context.componentId)
+    openShift.getLatestVersion(componentId)
   }
 
   private String getRolloutStatus(String replicationController) {
     openShift.getRolloutStatus(replicationController)
+  }
+  
+  private Map getPodDataForRollout(String replicationController) {
+    openShift.getPodDataForDeployment(componentId, replicationController)
   }
 }

src/org/ods/component/ScanWithSnykStage.groovy
@@ -15,7 +15,7 @@ class ScanWithSnykStage extends Stage {
       config.organisation = context.projectId
     }
     if (!config.projectName) {
-      config.projectName = context.componentId
+      config.projectName = componentId
     }
     if (!config.buildFile) {
       config.buildFile = 'build.gradle'
@@ -73,15 +73,15 @@ class ScanWithSnykStage extends Stage {
       }
     }
 
-    generateAndArchiveReport()
+    generateAndArchiveReport(context.localCheckoutEnabled)
 
     if (!testSuccess && config.failOnVulnerabilities) {
       script.error 'Snyk scan stage failed. See snyk report for details.'
     }
   }
 
-  private generateAndArchiveReport() {
-    def targetReport = "SCSR-${context.projectId}-${context.componentId}-${snyk.reportFile}"
+  private generateAndArchiveReport(boolean archive) {
+    def targetReport = "SCSR-${context.projectId}-${componentId}-${snyk.reportFile}"
     script.sh(
       label: 'Create artifacts dir',
       script: 'mkdir -p artifacts/SCSR'
@@ -90,9 +90,11 @@ class ScanWithSnykStage extends Stage {
       label: 'Rename report to SCSR',
       script: "mv ${snyk.reportFile} artifacts/${targetReport}"
     )
-    script.archiveArtifacts(artifacts: 'artifacts/SCSR*')
+    if (archive) {
+      script.archiveArtifacts(artifacts: 'artifacts/SCSR*')
+    }
     script.stash(
-      name: "scrr-report-${context.componentId}-${context.buildNumber}",
+      name: "scrr-report-${componentId}-${context.buildNumber}",
       includes: 'artifacts/SCSR*',
       allowEmpty: true
     )

src/org/ods/component/ScanWithSonarStage.groovy
@@ -42,7 +42,7 @@ class ScanWithSonarStage extends Stage {
 
     scan(sonarProperties)
 
-    generateAndArchiveReports(sonarProjectKey, context.buildTag)
+    generateAndArchiveReports(sonarProjectKey, context.buildTag, context.localCheckoutEnabled)
 
     if (config.requireQualityGatePass) {
       def qualityGateResult = getQualityGateResult(sonarProjectKey)
@@ -154,7 +154,7 @@ class ScanWithSonarStage extends Stage {
     return [:]
   }
 
-  private void generateAndArchiveReports(String projectKey, String author) {
+  private generateAndArchiveReports(String projectKey, String author, boolean archive) {
     def targetReport = "SCRR-${projectKey}.docx"
     def targetReportMd = "SCRR-${projectKey}.md"
     sonarQube.generateCNESReport(projectKey, author)
@@ -170,7 +170,9 @@ class ScanWithSonarStage extends Stage {
       label: 'Rename report to SCRR',
       script: "mv artifacts/*-analysis-report.docx* artifacts/${targetReport}; mv artifacts/*-analysis-report.md* artifacts/${targetReportMd}"
     )
-    script.archiveArtifacts(artifacts: 'artifacts/SCRR*')
+    if (archive) {
+      script.archiveArtifacts(artifacts: 'artifacts/SCRR*')
+    }
     script.stash(
       name: "scrr-report-${context.componentId}-${context.buildNumber}",
       includes: 'artifacts/SCRR*',

src/org/ods/component/Stage.groovy
@@ -4,6 +4,7 @@ class Stage {
   protected def script
   protected def context
   protected Map config
+  protected String componentId
 
   public final String STAGE_NAME = 'NOT SET'
 
@@ -11,11 +12,12 @@ class Stage {
     this.script = script
     this.context = context
     this.config = config
+    componentId = config.componentId ?: context.componentId
   }
 
   def execute() {
     // TODO: Replace withStage with simple echo calls once all stages use this class.
-    script.withStage(STAGE_NAME, context) {
+    script.withStage(STAGE_NAME + ' (' + componentId + ')', context) {
       this.run()
     }
   }

commit 2f5ff2fd2b0f9242859dffc2c6268760024ec651

Do not query for OpenShift app domain when no environment has been determined
Fixes bug introduced in #242.

src/org/ods/component/Pipeline.groovy
@@ -79,7 +79,7 @@ class Pipeline implements Serializable {
                         def registry = ServiceRegistry.instance
 
                         registry.add(GitService, new GitService(script))
-                        gitService = registry.get(GitService)
+                        this.gitService = registry.get(GitService)
 
                         registry.add(BitbucketService, new BitbucketService(
                             script,
@@ -87,12 +87,16 @@ class Pipeline implements Serializable {
                             context.projectId,
                             context.credentialsId
                         ))
-                        bitbucketService = registry.get(BitbucketService)
+                        this.bitbucketService = registry.get(BitbucketService)
 
-                        registry.add(OpenShiftService, new OpenShiftService(script, context.targetProject))
-                        
+                        registry.add(OpenShiftService, new OpenShiftService(
+                            script,
+                            context.targetProject
+                        ))
+                        this.openShiftService = registry.get(OpenShiftService)
+
                         registry.add(JenkinsService, new JenkinsService(script, logger))
-                        jenkinsService = registry.get(JenkinsService)
+                        this.jenkinsService = registry.get(JenkinsService)
                     }
 
                     skipCi = isCiSkip()
@@ -100,10 +104,14 @@ class Pipeline implements Serializable {
                         logger.info 'Skipping build due to [ci skip] in the commit message ...'
                         updateBuildStatus('NOT_BUILT')
                         setBitbucketBuildStatus('SUCCESSFUL')
-                    } else {
-                        context.setOpenshiftApplicationDomain (
-                            ServiceRegistry.instance.get(OpenShiftService).getOpenshiftApplicationDomain())
-                
+                        return
+                    }
+
+                    if (context.environment) {
+                        context.setOpenshiftApplicationDomain(
+                            openShiftService.getOpenshiftApplicationDomain()
+                        )
+
                         def autoCloneEnabled = !!context.cloneSourceEnv
                         if (autoCloneEnabled) {
                             createOpenShiftEnvironment(context)