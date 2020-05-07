62503e580da8d7b7427632b27270e41c51500a58
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-05-07 19:43:32 +0200

    use std jenkins magic

--- src/org/ods/component/UploadToNexusStage.groovy
+++ src/org/ods/component/UploadToNexusStage.groovy
@@ -10,7 +10,6 @@ class UploadToNexusStage extends Stage {
     final def distFile
     final def uploadPath
     final NexusService nexus
-    final def path
 
     UploadToNexusStage(def script, IContext context, Map config) {
         super(script, context, config)
@@ -18,7 +17,6 @@ class UploadToNexusStage extends Stage {
         this.distFile = config.distributionFile ?: "${componentId}-${context.tagversion}.tar.gz"
         def groupId  = config.groupId ?: context.groupId
         this.uploadPath = "${groupId.replace('.', '/')}/${componentId}/${context.tagversion}"
-        this.path = config.path ?: script.env.WORKSPACE
 
         nexus = new NexusService(context.nexusHost, context.nexusUsername, context.nexusPassword)
     }
@@ -26,12 +24,13 @@ class UploadToNexusStage extends Stage {
     def run() {
         script.echo ("Uploading '${distFile}' to: ${uploadPath}")
 
-        if (!script.fileExists ("${path}/${distFile}")) {
-            script.error ("Could not upload file '${path}/${distFile}' - it does NOT exist!")
+        if (!script.fileExists (distFile)) {
+            script.error ("Could not upload file ${distFile} - it does NOT exist!")
         }
-
-        return nexus.storeArtifactFromFile(repoType, uploadPath, distFile, 
-            new File("${path}/${distFile}"), "application/octet-stream")
+        def uploadUri = nexus.storeArtifactFromFile(repoType, uploadPath, distFile,
+            script.readFile(distFile), "application/octet-stream")
+        script.echo "Uploaded '${distFile}' to '${uploadUri}'"
+        return uploadUri
     }
 
 }
