50a7d31935b565f65a561090fa25b41a4666996b
René Dupont <rene.dupont@boehringer-ingelheim.com>
2020-05-08 16:32:39 +0200

    added pipefail to snyk scripts

--- src/org/ods/services/SnykService.groovy
+++ src/org/ods/services/SnykService.groovy
@@ -16,37 +16,54 @@ class SnykService {
 
     boolean version() {
         script.sh(
-          script: "snyk version | tee -a ${reportFile}",
-          returnStatus: true,
-          label: 'Get Snyk version'
+            script: """
+            set -e
+            set -o pipefail
+            snyk version | tee -a ${reportFile}
+            """,
+            returnStatus: true,
+            label: 'Get Snyk version'
         ) == 0
     }
 
     boolean auth(String authCode) {
         script.sh(
-          script: "snyk auth ${authCode} | tee -a ${reportFile}",
-          returnStatus: true,
-          label: 'Authenticate with Snyk server'
+            script: """
+            set -e
+            set -o pipefail
+            snyk auth ${authCode} | tee -a ${reportFile}
+          """,
+            returnStatus: true,
+            label: 'Authenticate with Snyk server'
         ) == 0
     }
 
     boolean test(String organisation, String buildFile, String severityThreshold) {
         script.sh(
-          script: """
-          snyk test \
+            script: """
+            set -e
+            set -o pipefail
+            snyk test \
             --org=${organisation} \
             --file=${buildFile} \
             --all-sub-projects \
             --severity-threshold=${severityThreshold} | tee -a ${reportFile}
           """,
-          returnStatus: true,
-          label: 'Run Snyk test'
+            returnStatus: true,
+            label: 'Run Snyk test'
         ) == 0
     }
 
     boolean monitor(String organisation, String buildFile) {
         script.sh(
-            script: "snyk monitor --org=${organisation} --file=${buildFile} --all-sub-projects | tee -a ${reportFile}",
+            script: """
+              set -e
+              set -o pipefail
+              snyk monitor \
+              --org=${organisation} \
+              --file=${buildFile} \
+              --all-sub-projects | tee -a ${reportFile}
+            """,
             returnStatus: true,
             label: 'Start monitoring in snyk.io'
         ) == 0
