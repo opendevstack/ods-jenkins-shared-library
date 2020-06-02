e9a1b536921932e7f4765a886d362e57da13463c
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-06-02 10:08:53 +0200

    add return force

--- vars/withStage.groovy
+++ vars/withStage.groovy
@@ -5,7 +5,9 @@ def call(String stageLabel, def context, ILogger logger, Closure stageClosure) {
         "**** STARTING stage '${stageLabel}' for component '${context.componentId}' " +
         "branch '${context.gitBranch}' ****")
     try {
-        return result = stage(stageLabel) { stageClosure() }
+        def result
+        stage(stageLabel) { result = stageClosure() }
+        return result
     } finally {
         logger.infoClocked("${stageLabel}-${context.componentId}",
             "**** ENDED stage '${stageLabel}' for component '${context.componentId}' " +
