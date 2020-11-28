2adaf4a181f6919a268992cc7733cae8565ae8df
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2020-11-28 20:06:27 +0100

    Fix querying of Jira issue data by current version

--- src/org/ods/orchestration/util/Project.groovy
+++ src/org/ods/orchestration/util/Project.groovy
@@ -866,7 +866,10 @@ class Project {
 
     DocumentHistory getHistoryForDocument(String document) {
         this.steps.echo("??? getHistoryForDocument for doc ${document}")
-        this.steps.echo(this.data.documentHistories.each { doc, hist -> "${doc}: ${hist}" })
+        this.data.documentHistories.each { doc, hist ->
+            this.steps.echo("${doc}")
+            this.steps.echo("${hist}")
+        }
         return this.data.documentHistories.getOrDefault(document, [:])
     }
 
