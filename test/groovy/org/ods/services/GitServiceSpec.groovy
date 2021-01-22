bf41b5e7c3c995f39085191a07e264adba4e9487
SimonGolms <development@gol.ms>
2021-01-22 17:11:50 +0100

    test(GitService): add test cases for skipping commit message

--- test/groovy/org/ods/services/GitServiceSpec.groovy
+++ test/groovy/org/ods/services/GitServiceSpec.groovy
@@ -18,5 +18,56 @@ class GitServiceSpec extends SpecHelper {
         then:
         releaseBranch == "release/0.0.1"
     }
-}
 
+    def "git commit with skip patern: [ci skip]"() {
+        given:
+        def gitCommitMessage = "docs: update README [ci skip]"
+        def script = new PipelineScript()
+        def service = new GitService(script, new Logger(script, false))
+
+        when:
+        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
+
+        then:
+        isSkippingCommitMessage == true
+    }
+
+    def "git commit with skip patern: [skip ci]"() {
+        given:
+        def gitCommitMessage = "docs: update README [skip ci]"
+        def script = new PipelineScript()
+        def service = new GitService(script, new Logger(script, false))
+
+        when:
+        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
+
+        then:
+        isSkippingCommitMessage == true
+    }
+
+    def "git commit with skip patern: ***NO_CI***"() {
+        given:
+        def gitCommitMessage = "docs: update README ***NO_CI***"
+        def script = new PipelineScript()
+        def service = new GitService(script, new Logger(script, false))
+
+        when:
+        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
+
+        then:
+        isSkippingCommitMessage == true
+    }
+
+    def "git commit without skip patern"() {
+        given:
+        def gitCommitMessage = "docs: update README"
+        def script = new PipelineScript()
+        def service = new GitService(script, new Logger(script, false))
+
+        when:
+        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
+
+        then:
+        isSkippingCommitMessage == false
+    }
+}
