eee89fa0dd1fe36bfd78fb17b496425560bd1c4a
SimonGolms <development@gol.ms>
2021-01-27 10:09:56 +0100

    test(GitService): refactor and condense tests cases
    - use where tables to test different scenarios

--- test/groovy/org/ods/services/GitServiceSpec.groovy
+++ test/groovy/org/ods/services/GitServiceSpec.groovy
@@ -19,72 +19,23 @@ class GitServiceSpec extends SpecHelper {
         releaseBranch == "release/0.0.1"
     }
 
-    def "git commit with skip pattern: [ci skip]"() {
+    def "git skipping commit message"() {
         given:
-        def gitCommitMessage = "docs: update README [ci skip]"
         def script = new PipelineScript()
         def service = new GitService(script, new Logger(script, false))
 
         when:
-        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
+        def result = service.isCiSkipInCommitMessage(gitCommitMessage)
 
         then:
-        isSkippingCommitMessage == true
-    }
+        result == isSkippingCommitMessage
 
-    def "git commit with skip pattern: [skip ci]"() {
-        given:
-        def gitCommitMessage = "docs: update README [skip ci]"
-        def script = new PipelineScript()
-        def service = new GitService(script, new Logger(script, false))
-
-        when:
-        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
-
-        then:
-        isSkippingCommitMessage == true
-    }
-
-    def "git commit with skip pattern: ***NO_CI***"() {
-        given:
-        def gitCommitMessage = "docs: update README ***NO_CI***"
-        def script = new PipelineScript()
-        def service = new GitService(script, new Logger(script, false))
-
-        when:
-        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
-
-        then:
-        isSkippingCommitMessage == true
-    }
-
-    def "git commit without skip pattern"() {
-        given:
-        def gitCommitMessage = "docs: update README"
-        def script = new PipelineScript()
-        def service = new GitService(script, new Logger(script, false))
-
-        when:
-        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
-
-        then:
-        isSkippingCommitMessage == false
-    }
-
-    def "git commit multiline including skip pattern"() {
-        given:
-        def gitCommitMessage = """docs: update README
-
-        - typo
-        - [ci skip]
-        """
-        def script = new PipelineScript()
-        def service = new GitService(script, new Logger(script, false))
-
-        when:
-        def isSkippingCommitMessage = service.isCiSkipInCommitMessage(gitCommitMessage)
-
-        then:
-        isSkippingCommitMessage == false
+        where:
+        gitCommitMessage                             || isSkippingCommitMessage
+        'docs: update README [ci skip]'              || true
+        'docs: update README [skip ci]'              || true
+        'docs: update README ***NO_CI***'            || true
+        'docs: update README'                        || false
+        'docs: update README\n\n- typo\n- [ci skip]' || false
     }
 }
