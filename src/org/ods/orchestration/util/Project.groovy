007c8a6bf786137fffdaf31e04d44b2a659c2e61
Jorge Romero <jorge.romero.ext@boehringer-ingelheim.com>
2021-05-03 18:10:32 +0200

    Feature/id in test cycle results (#638)
    * Change the way the test cycle name is created
    
    * Change message
    
    * Unit test
    
    * Add changelog entry
    
    * Fix codenarc violations

--- src/org/ods/orchestration/util/Project.groovy
+++ src/org/ods/orchestration/util/Project.groovy
@@ -1200,7 +1200,7 @@ class Project {
         loadVersionDataFromJira(this.buildParams.version)
     }
 
-    protected Map loadVersionDataFromJira(String versionName) {
+    Map loadVersionDataFromJira(String versionName) {
         if (!this.jiraUseCase) return [:]
         if (!this.jiraUseCase.jira) return [:]
 
