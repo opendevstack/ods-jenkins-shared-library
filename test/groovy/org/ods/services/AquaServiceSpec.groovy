eb14ebc1b54fc47b8ef152f7fac80ccff1375958
zxBCN Rodriguez_Cornejo,Hector (IT EDS) EXTERNAL <hector.rodriguez_cornejo.ext@boehringer-ingelheim.com>
2021-06-04 12:18:09 +0200

    Added tests of AquaService

--- test/groovy/org/ods/services/AquaServiceSpec.groovy
+++ test/groovy/org/ods/services/AquaServiceSpec.groovy
@@ -27,4 +27,40 @@ class AquaServiceSpec extends PipelineSpockTestBase {
         1 * steps.withCredentials([[['USERNAME':'user'], ['PASSWORD':'pass']]], _)
     }
 
+    def "invoke Aqua cli"() {
+        given:
+        def steps = Spy(util.PipelineSteps)
+        def service = Spy(AquaService, constructorArgs: [
+            steps,
+            new Logger(steps, false)
+        ])
+
+        when:
+        def result = service.scanViaCli("http://aqua", "internal", "12345",
+            "cd-user", "report.html", "report.json")
+
+        then:
+        2 * steps.getEnv() >> ['USERNAME':'user', 'PASSWORD': 'pass']
+        1 * steps.sh(_) >> {
+            assert it.label == ['Scan via Aqua CLI']
+            assert it.returnStatus == [true]
+            assert it.script.toString().contains('set +e &&')
+            assert it.script.toString().contains('aquasec scan 12345')
+            assert it.script.toString().contains('--dockerless')
+            assert it.script.toString().contains('--register')
+            assert it.script.toString().contains('--text')
+            assert it.script.toString().contains('--htmlfile \'report.html\'')
+            assert it.script.toString().contains('--jsonfile \'report.json\'')
+            assert it.script.toString().contains('-w /tmp')
+            assert it.script.toString().contains('-U \'user\'')
+            assert it.script.toString().contains('-P \'pass\'')
+            assert it.script.toString().contains('-H \'http://aqua\'')
+            assert it.script.toString().contains('--registry \'internal\' &&')
+            assert it.script.toString().contains('set -e')
+
+            return 0
+        }
+        0 == result
+    }
+
 }
