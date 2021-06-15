75ab315a34e2b26bfc02e7168406ce5af0d95e9f
zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
2021-06-15 17:17:33 +0200

    Fix codenarc

--- src/org/ods/orchestration/util/ProjectMessagesUtil.groovy
+++ src/org/ods/orchestration/util/ProjectMessagesUtil.groovy
@@ -3,7 +3,9 @@ package org.ods.orchestration.util
 import org.ods.orchestration.usecase.LeVADocumentUseCase
 
 class ProjectMessagesUtil {
-     static String generateWIPIssuesMessage(Project project) {
+
+    @SuppressWarnings('Instanceof')
+    static String generateWIPIssuesMessage(Project project) {
         def message = project.isWorkInProgress ? 'Pipeline-generated documents are watermarked ' +
             "'${LeVADocumentUseCase.WORK_IN_PROGRESS_WATERMARK}' " +
             'since the following issues are work in progress: ' :
