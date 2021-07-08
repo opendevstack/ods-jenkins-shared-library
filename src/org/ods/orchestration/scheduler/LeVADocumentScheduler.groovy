c51c97c3c5dacf981e9ebea3f77470230de24d91
zxBCN Rodriguez_Cornejo,Hector (IT EDS) EXTERNAL <hector.rodriguez_cornejo.ext@boehringer-ingelheim.com>
2021-07-08 14:35:43 +0200

    CodeNarc

--- src/org/ods/orchestration/scheduler/LeVADocumentScheduler.groovy
+++ src/org/ods/orchestration/scheduler/LeVADocumentScheduler.groovy
@@ -215,7 +215,7 @@ class LeVADocumentScheduler extends DocGenScheduler {
         }
 
         // Applicable for certain document types only if the Jira service is configured in the release manager configuration
-        if ([LeVADocumentUseCase.DocumentType.CSD, LeVADocumentUseCase.DocumentType.SSDS, LeVADocumentUseCase.DocumentType.CFTP, LeVADocumentUseCase.DocumentType.CFTR,LeVADocumentUseCase.DocumentType.IVP,LeVADocumentUseCase.DocumentType.IVR,LeVADocumentUseCase.DocumentType.DIL, LeVADocumentUseCase.DocumentType.TCP, LeVADocumentUseCase.DocumentType.TCR, LeVADocumentUseCase.DocumentType.RA, LeVADocumentUseCase.DocumentType.TRC].contains(documentType as LeVADocumentUseCase.DocumentType)) {
+        if ([LeVADocumentUseCase.DocumentType.CSD, LeVADocumentUseCase.DocumentType.SSDS, LeVADocumentUseCase.DocumentType.CFTP, LeVADocumentUseCase.DocumentType.CFTR, LeVADocumentUseCase.DocumentType.IVP, LeVADocumentUseCase.DocumentType.IVR, LeVADocumentUseCase.DocumentType.DIL, LeVADocumentUseCase.DocumentType.TCP, LeVADocumentUseCase.DocumentType.TCR, LeVADocumentUseCase.DocumentType.RA, LeVADocumentUseCase.DocumentType.TRC].contains(documentType as LeVADocumentUseCase.DocumentType)) {
             result = result && this.project.services?.jira != null
         }
 
