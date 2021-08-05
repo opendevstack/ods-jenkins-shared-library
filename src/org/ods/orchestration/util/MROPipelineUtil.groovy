b4fff3ab4c1171229c0d75a7cb906501fd084d1c
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2021-08-05 14:24:29 +0200

    debug on why file not found

--- src/org/ods/orchestration/util/MROPipelineUtil.groovy
+++ src/org/ods/orchestration/util/MROPipelineUtil.groovy
@@ -93,7 +93,8 @@ class MROPipelineUtil extends PipelineUtil {
                 logger.info("Repository '${repo.id}' is in sync with OpenShift, no need to rebuild")
                 return
             }
-
+            this.steps.sh("git log -1")
+            this.steps.sh("ls -la")
             def job
             def env = []
             env.addAll(this.project.getMainReleaseManagerEnv())
