3acd021bfd5d2814d0f2470dfbf6028dce45fb22
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2021-08-05 15:48:57 +0200

    codenarc

--- src/org/ods/orchestration/InitStage.groovy
+++ src/org/ods/orchestration/InitStage.groovy
@@ -28,7 +28,7 @@ class InitStage extends Stage {
         super(script, project, repos, startAgentStageName)
     }
 
-    @SuppressWarnings(['CyclomaticComplexity', 'NestedBlockDepth', 'GStringAsMapKey', 'LineLength'])
+    @SuppressWarnings(['CyclomaticComplexity', 'NestedBlockDepth', 'GStringAsMapKey', 'LineLength', 'Indentation'])
     def run() {
         ILogger logger = ServiceRegistry.instance.get(Logger)
         def steps = new PipelineSteps(script)
