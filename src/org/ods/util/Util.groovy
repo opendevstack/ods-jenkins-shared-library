ded71527dd54f0b89d1d725570efb82a77f684f8
Clemens Utschig <40628552+clemensutschig@users.noreply.github.com>
2021-11-05 17:27:44 +0100

    codenarc findings

--- src/org/ods/util/Util.groovy
+++ src/org/ods/util/Util.groovy
@@ -3,27 +3,28 @@ package org.ods.util
 class Util {
     static def findAll(Collection c, Closure filter) {
         def results = []
-    
+
         for (def i = 0; i < c.size(); i++) {
             if (filter(c[i])) {
                 results.add(c[i])
             }
         }
-        
         return results
     }
 
     static def collectEntries(Collection c, Closure key, Closure value) {
         def results = [:]
-    
+
         for (def i = 0; i < c.size(); i++) {
             results[key(c[i])] = value(c[i])
         }
-        
+
         return results
     }
 
-    static def executeAndRetryOnNonSerializableException(def script, Closure retryCondition, Closure block, int retries = 5, int waitTime = 5) {
+    @SuppressWarnings('VariableName')
+    static def executeAndRetryOnNonSerializableException(def script, Closure retryCondition,
+        Closure block, int retries = 5, int waitTime = 5) {
         final def maxRetries = retries
 
         while (retryCondition() && retries-- > 0) {
