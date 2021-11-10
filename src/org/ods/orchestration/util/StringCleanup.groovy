c9760a9faaf8d1543716e03f7093ca2e9394bbd2
zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
2021-11-10 16:48:05 +0100

    Add noncps to StringCleanup.removeCharacters method

--- src/org/ods/orchestration/util/StringCleanup.groovy
+++ src/org/ods/orchestration/util/StringCleanup.groovy
@@ -1,10 +1,13 @@
 package org.ods.orchestration.util
 
+import com.cloudbees.groovy.cps.NonCPS
+
 class StringCleanup {
     static CHARACTER_REMOVEABLE = [
        '\u00A0' : ' '
     ]
 
+    @NonCPS
     static removeCharacters(inputString) {
         def outputString = inputString
         CHARACTER_REMOVEABLE.forEach{k,v ->
