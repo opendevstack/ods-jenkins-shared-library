409686c5aab77e2227cfd4507fe9bfe70bf44fc7
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2021-11-12 11:16:20 +0100

    codenarc

--- src/org/ods/orchestration/FinalizeStage.groovy
+++ src/org/ods/orchestration/FinalizeStage.groovy
@@ -181,9 +181,8 @@ class FinalizeStage extends Stage {
         for (def i = 0; i < repoSize; i++) {
             def repo = flattenedRepos[i]
             if (repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_TEST &&
-                repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_INFRA&&
-                repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_SAAS_SERVICE )
-            {
+                repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_INFRA &&
+                repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_SAAS_SERVICE ) {
                 repoIntegrateTasks << [ (repo.id): {
                     steps.dir("${steps.env.WORKSPACE}/${MROPipelineUtil.REPOS_BASE_DIR}/${repo.id}") {
                         def filesToCheckout = []
