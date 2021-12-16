bcd007ab874719eadd6c9832ab4fb68610ae9906
Clemens Utschig <40628552+clemensutschig@users.noreply.github.com>
2021-12-16 09:08:48 +0100

    emergency fix - wrong file pattern

--- src/org/ods/services/JenkinsService.groovy
+++ src/org/ods/services/JenkinsService.groovy
@@ -44,7 +44,7 @@ class JenkinsService {
 
         def foundTests = 0
         script.dir (XUNIT_SYSTEM_RESULT_DIR) {
-            foundTests = script.findFiles(glob : '**/.xml').size()
+            foundTests = script.findFiles(glob : '**/**.xml').size()
         }
 
         logger.debug "Found ${foundTests} test files in '${XUNIT_SYSTEM_RESULT_DIR}'"
