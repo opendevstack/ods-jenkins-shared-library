02d70bcd9797062750b15102ccef53d9c9085676
Angel MP <angelmp.mail@gmail.com>
2022-02-02 14:57:20 +0100

    Set test summary for empty description in TCR (#837)
    * Set test summary for empty description in TCR and added getTestDescription test

--- src/org/ods/orchestration/usecase/LeVADocumentUseCase.groovy
+++ src/org/ods/orchestration/usecase/LeVADocumentUseCase.groovy
@@ -847,7 +847,7 @@ class LeVADocumentUseCase extends DocGenUseCase {
                 integrationTests    : SortUtil.sortIssuesByKey(integrationTestIssues.collect { testIssue ->
                     [
                         key         : testIssue.key,
-                        description : this.convertImages(testIssue.description ?: ''),
+                        description : this.convertImages(getTestDescription(testIssue)),
                         requirements: testIssue.requirements ? testIssue.requirements.join(", ") : "N/A",
                         isSuccess   : testIssue.isSuccess,
                         bugs        : testIssue.bugs ? testIssue.bugs.join(", ") : (testIssue.comment ? "": "N/A"),
