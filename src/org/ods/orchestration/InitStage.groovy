9de5a02f27c5061a948001858bb8f3234c8efec6
zxBCN Pablos_Ceruelo,Victor (IT EDS) EXTERNAL <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-02-25 15:11:42 +0100

    Default branch when in WIP.

--- src/org/ods/orchestration/InitStage.groovy
+++ src/org/ods/orchestration/InitStage.groovy
@@ -47,7 +47,7 @@ class InitStage extends Stage {
         logger.startClocked("git-releasemanager-${STAGE_NAME}")
         // git checkout
         def gitReleaseBranch = GitService.getReleaseBranch(buildParams.version)
-        def gitReleaseManagerBranch = "*/${gitReleaseBranch}"
+        def gitReleaseManagerBranch = "refs/heads/master"
         if (!Project.isWorkInProgress(buildParams.version)) {
             if (Project.isPromotionMode(buildParams.targetEnvironmentToken)) {
                 def tagList = git.readBaseTagList(
