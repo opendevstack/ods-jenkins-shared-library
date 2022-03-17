62414b003fbb979a09bb60fccaaa8bc82ec6697e
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-03-17 19:45:13 +0100

    codenarc fixes. See: https://codenarc.org/codenarc-rules-formatting.html#SpaceAroundMapEntryColon

--- src/org/ods/orchestration/mapper/LeVADocumentParamsMapper.groovy
+++ src/org/ods/orchestration/mapper/LeVADocumentParamsMapper.groovy
@@ -12,45 +12,45 @@ class LeVADocumentParamsMapper {
 
     Map build(Map data = [:]) {
         return [
-            build    : mapBuildData(),
-            git      : mapGitData(),
-            openshift: [
-                targetApiUrl: this.project.getOpenShiftApiUrl() //TODO is different?
+            build:mapBuildData(),
+            git:mapGitData(),
+            openshift:[
+                targetApiUrl:this.project.getOpenShiftApiUrl() //TODO is different?
             ]
         ] << data
     }
 
     private Map<String, Object> mapGitData() {
         [
-            commit              : this.project.gitData.commit,
-            repoURL             : this.project.gitData.url,
-            releaseManagerRepo  : this.project.gitData.releaseManagerRepo,
-            releaseManagerBranch: this.project.data.gitReleaseManagerBranch,
-            baseTag             : this.project.gitData.baseTag,
-            targetTag           : this.project.gitData.targetTag,
-            author              : this.project.gitData.author,
-            message             : this.project.gitData.message,
-            time                : this.project.gitData.time,
+            commit:this.project.gitData.commit,
+            repoURL:this.project.gitData.url,
+            releaseManagerRepo:this.project.gitData.releaseManagerRepo,
+            releaseManagerBranch:this.project.data.gitReleaseManagerBranch,
+            baseTag:this.project.gitData.baseTag,
+            targetTag:this.project.gitData.targetTag,
+            author:this.project.gitData.author,
+            message:this.project.gitData.message,
+            time:this.project.gitData.time,
         ]
     }
 
     private LinkedHashMap<String, Object> mapBuildData() {
         [
-            targetEnvironment        : this.project.buildParams.targetEnvironment,
-            targetEnvironmentToken   : this.project.buildParams.targetEnvironmentToken,
-            version                  : this.project.buildParams.version,
-            configItem               : this.project.buildParams.configItem,
-            changeDescription        : this.project.buildParams.changeDescription,
-            changeId                 : this.project.buildParams.changeId,
-            rePromote                : this.project.buildParams.rePromote,
-            releaseStatusJiraIssueKey: this.project.buildParams.releaseStatusJiraIssueKey,
-            runDisplayUrl            : this.project.steps.env.RUN_DISPLAY_URL,
-            releaseParamVersion      : this.project.steps.env.RELEASE_PARAM_VERSION,
-            buildId                  : this.project.steps.env.BUILD_ID, // TODO is different?
-            buildURL                 : this.project.steps.env.BUILD_URL, // TODO is different?
-            jobName                  : this.project.steps.env.JOB_NAME,
-            testResultsURLs          : this.project.data.build.testResultsURLs,
-            jenkinLog                : this.project.data.jenkinLog,
+            targetEnvironment:this.project.buildParams.targetEnvironment,
+            targetEnvironmentToken:this.project.buildParams.targetEnvironmentToken,
+            version:this.project.buildParams.version,
+            configItem:this.project.buildParams.configItem,
+            changeDescription:this.project.buildParams.changeDescription,
+            changeId:this.project.buildParams.changeId,
+            rePromote:this.project.buildParams.rePromote,
+            releaseStatusJiraIssueKey:this.project.buildParams.releaseStatusJiraIssueKey,
+            runDisplayUrl:this.project.steps.env.RUN_DISPLAY_URL,
+            releaseParamVersion:this.project.steps.env.RELEASE_PARAM_VERSION,
+            buildId:this.project.steps.env.BUILD_ID, // TODO is different?
+            buildURL:this.project.steps.env.BUILD_URL, // TODO is different?
+            jobName:this.project.steps.env.JOB_NAME,
+            testResultsURLs:this.project.data.build.testResultsURLs,
+            jenkinLog:this.project.data.jenkinLog,
         ]
     }
 
