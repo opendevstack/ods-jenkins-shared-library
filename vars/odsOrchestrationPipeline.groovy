afcb1bc4686a4e9a270d9e1145528d349f03fb63
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2022-03-20 20:14:54 +0100

    nothing but return

--- vars/odsOrchestrationPipeline.groovy
+++ vars/odsOrchestrationPipeline.groovy
@@ -26,8 +26,11 @@ import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
-
 def call (Map config) {
+    echo 'nothing'
+}
+
+def YYYcall (Map config) {
     echo "${env.JOB_NAME} / ${env.BUILD_NUMBER}"
     def thisJob = env.JOB_NAME
     List<WorkflowJob> jobs = Jenkins.getActiveInstance().getAllItems(WorkflowJob.class);
@@ -39,7 +42,6 @@ def call (Map config) {
             }
         }
     }
-    UnirestConfig.init()    
 }
 
 @SuppressWarnings('AbcMetric')
