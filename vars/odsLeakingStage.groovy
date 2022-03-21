1ba821c40a63c7d8b8d5cf1ca2d5b4a58ed9f612
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2022-03-21 11:04:26 +0100

    simplify

--- vars/odsLeakingStage.groovy
+++ vars/odsLeakingStage.groovy
@@ -10,22 +10,25 @@ import org.ods.util.UnirestConfig
 def call (Map config, Closure stages = null) {
     UnirestConfig.init()
     def debug = config.get('debug', false)
+    if (!config.get('return', false)) {
+        echo 'returning ...'
+        return
+    }
+
     ServiceRegistry.instance.add(Logger, new Logger(this, debug))
     ILogger logger = ServiceRegistry.instance.get(Logger)
-    logger.debug('here')
+    logger.debug('here (master')
     try {
         node ('master') {
-            if (!config.get('return', false)) {
-                writeFile(
-                    file: "dummy.groovy",
-                    text: "@Library('ods-jenkins-shared-library@fix/extract_enums') _ \n" +
-                        "echo 'dynamic' \n" + 
-                        "odsLeakingStage(return : true) \n")
-                def data = readFile (file: 'dummy.groovy')
-                echo "Created script: \n ${data}"
-                load ('dummy.groovy')
-                new ThrowingStage(this).execute(stages)
-            } 
+            writeFile(
+                file: "dummy.groovy",
+                text: "@Library('ods-jenkins-shared-library@fix/extract_enums') _ \n" +
+                    "echo 'dynamic' \n" + 
+                    "odsLeakingStage(return : true) \n")
+            def data = readFile (file: 'dummy.groovy')
+            echo "Created script: \n ${data}"
+            load ('dummy.groovy')
+            new ThrowingStage(this).execute(stages)
         }
     } finally {
         // clear it all ...
