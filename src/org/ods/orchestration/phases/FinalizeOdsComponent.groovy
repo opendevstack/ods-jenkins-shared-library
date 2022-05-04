5617bd745b50ecd8de7fe8d60b5cfd9aad3eb076
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2022-05-04 10:43:23 +0200

    allComponentDeployments != null

--- src/org/ods/orchestration/phases/FinalizeOdsComponent.groovy
+++ src/org/ods/orchestration/phases/FinalizeOdsComponent.groovy
@@ -131,7 +131,7 @@ class FinalizeOdsComponent {
             allComponentDeployments.remove(odsBuiltDeploymentName)
         }
 
-        if (allComponentDeployments && allComponentDeployments.size() > 0 ) {
+        if (allComponentDeployments != null && allComponentDeployments.size() > 0 ) {
             def message = "DeploymentConfigs (component: '${repo.id}') found that are not ODS managed: " +
                 "'${allComponentDeployments}'!\r" +
                 "Please fix by rolling them out through 'odsComponentStageRolloutOpenShiftDeployment()'!"
