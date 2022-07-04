42ad47eb842a05580968ee88443109df943e65ae
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2022-07-04 13:58:44 +0200

    add split on path

--- src/org/ods/orchestration/phases/DeployOdsComponent.groovy
+++ src/org/ods/orchestration/phases/DeployOdsComponent.groovy
@@ -115,7 +115,7 @@ class DeployOdsComponent {
         if (!files || files.size() == 0) {
             throw new RuntimeException("Error: Could not determine starting directory. Neither of [chart, openshift, openshift-exported] found.")
         } else {
-            return files[0].path
+            return files[0].path.split('/')[0]
         } 
     }
 
