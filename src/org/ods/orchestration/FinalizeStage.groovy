f22903d5848917a7e94b42542a0fb40b34a39631
zxBCN Pablos_Ceruelo,Victor (IT EDS) EXTERNAL <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-07-18 13:56:07 +0000

    typo

--- src/org/ods/orchestration/FinalizeStage.groovy
+++ src/org/ods/orchestration/FinalizeStage.groovy
@@ -145,7 +145,7 @@ class FinalizeStage extends Stage {
             repoPushTasks << [ (repo.id): {
                 steps.dir("${steps.env.WORKSPACE}/${MROPipelineUtil.REPOS_BASE_DIR}/${repo.id}") {
                     if (project.isWorkInProgress) {
-                        String branchName = repo.data.git ?: repo.branch
+                        String branchName = repo.data.git.branch ?: repo.branch
                         git.pushRef(branchName)
                     } else if (project.isAssembleMode) {
                         git.createTag(project.targetTag)
