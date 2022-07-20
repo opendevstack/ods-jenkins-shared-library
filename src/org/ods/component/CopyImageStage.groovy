cb6f5b8c2d374419347d21f3575b7bdc72693bdd
Martin Marcher <serverhorror@users.noreply.github.com>
2022-07-20 14:28:50 +0000

    add importImageIntoProject method

--- src/org/ods/component/CopyImageStage.groovy
+++ src/org/ods/component/CopyImageStage.groovy
@@ -69,7 +69,88 @@ class CopyImageStage extends Stage {
     // This is called from Stage#execute if the branch being built is eligible.
     protected run() {
         logger.info("Copy the image ${options.sourceImageUrlIncludingRegistry}!")
-        logger.info("options: ${options}")
+        importImageIntoProject(context, options.sourceImageUrlIncludingRegistry, options.tagIntoTargetEnv, options.sourceCredential)
+    }
+
+    def importImageIntoProject(def context, String sourceImageUrlIncludingRegistry, boolean tagIntoTargetEnv = false, String sourceCredential = null) {
+        String STR_DOCKER_PROTOCOL = 'docker://'
+        def sourceImageData = sourceImageUrlIncludingRegistry?.minus(STR_DOCKER_PROTOCOL)?.split('/')
+        def sourceImageDataSplitLength = sourceImageData?.size()
+
+        // [REGISTRY/]REPO/IMAGE[:TAG]
+        def sourceImageInfo = [ : ]
+        if (sourceImageDataSplitLength >= 2) {
+            sourceImageInfo.image = sourceImageData[sourceImageDataSplitLength-1]
+            sourceImageInfo.repo = sourceImageData[sourceImageDataSplitLength-2]
+            // no registry info, use internal cluster registry
+            if (sourceImageDataSplitLength == 2) {
+                sourceImageInfo.registry = context.clusterRegistryAddress
+            } else {
+                sourceImageInfo.registry = sourceImageData[0..sourceImageDataSplitLength-3].join('/')
+            }
+            sourceImageInfo.registry = STR_DOCKER_PROTOCOL + sourceImageInfo.registry
+        } else {
+            error ("Incoming ${sourceImageUrlIncludingRegistry} is wrong. It must contain AT minimum the repo and image.\n" +
+                                        'Desired format: [REGISTRY/]REPO/IMAGE[:TAG]')
+        }
+
+        // amend the image info with the 'latest' tag if not passed
+        if (!sourceImageInfo.image.contains(':')) {
+            sourceImageInfo.image += ':latest'
+            logger.info("No image-tag found, amended to ${sourceImageInfo.image}")
+        }
+
+        logger.info("Resolved source Image data: ${sourceImageUrlIncludingRegistry} into ${sourceImageInfo},\n" +
+            "importing into: ${STR_DOCKER_PROTOCOL}${context.clusterRegistryAddress}/${context.cdProject}/${sourceImageInfo.image}")
+
+        // to make this work, we need a target image stream first
+        def openShiftService = ServiceRegistry.instance.get(OpenShiftService)
+        openShiftService.findOrCreateImageStream("${context.cdProject}", "${sourceImageInfo.image.split(':').first()}")
+
+        // read the target registry auth (for this project's -cd project ..)
+        def targetInternalRegistryToken = readFile '/run/secrets/kubernetes.io/serviceaccount/token'
+
+        def sourcetoken = sourceCredential ? "--src-creds ${sourceCredential}" : ''
+
+        try {
+            sh """
+            set +x
+            skopeo copy --src-tls-verify=false ${sourcetoken} \
+            ${sourceImageInfo.registry}/${sourceImageInfo.repo}/${sourceImageInfo.image} \
+            --dest-creds openshift:${targetInternalRegistryToken} \
+            ${STR_DOCKER_PROTOCOL}${context.clusterRegistryAddress}/${context.cdProject}/${sourceImageInfo.image} \
+            --dest-tls-verify=false
+            """
+        } catch (err) {
+            throw err
+        }
+
+        def imageName = sourceImageInfo.image.split(':').first()
+        def imageTag = sourceImageInfo.image.split(':').last()
+
+        def internalImageRef = openShiftService.getImageReference(context.cdProject, imageName, imageTag)
+
+        logger.info("!!! Image successfully imported into ${context.clusterRegistryAddress}/${context.cdProject}/${sourceImageInfo.image},\n" +
+            "details: ${internalImageRef}")
+
+        def buildResourceName = "imported-${imageName}"
+
+        context.addBuildToArtifactURIs("${buildResourceName}",
+            [image: internalImageRef, source: sourceImageUrlIncludingRegistry])
+
+        if (tagIntoTargetEnv) {
+
+            openShiftService.findOrCreateImageStream(context.targetProject, imageName)
+            openShiftService.importImageTagFromProject(
+                context.targetProject, imageName, context.cdProject,
+                imageTag, imageTag
+            )
+            logger.info("!!! Image ${sourceImageInfo.image} tagged into ${context.targetProject}")
+        }
+        logger.warn("NOT SCANNING IMAGES -- odsComponentStageScanWithAqua(context, [resourceName: \"${buildResourceName}\", imageLabels : context.extensionImageLabels])")
+        // this needs to go into the new stage ...  (similar to the odsBuild...stage)
+        // odsComponentStageScanWithAqua(context, [resourceName: "${buildResourceName}", imageLabels : context.extensionImageLabels])
+
     }
 
     protected String stageLabel() {
