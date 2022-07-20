51608eb958aff17eebe551ff5aecb495e432fd89
Martin Marcher <serverhorror@users.noreply.github.com>
2022-07-20 13:41:04 +0000

    move to info level for now

--- src/org/ods/component/CopyImageStage.groovy
+++ src/org/ods/component/CopyImageStage.groovy
@@ -68,8 +68,8 @@ class CopyImageStage extends Stage {
 
     // This is called from Stage#execute if the branch being built is eligible.
     protected run() {
-        logger.debug("Copy the image ${options.sourceImageUrlIncludingRegistry}!")
-        logger.debug("options: ${options}")
+        logger.info("Copy the image ${options.sourceImageUrlIncludingRegistry}!")
+        logger.info("options: ${options}")
     }
 
     protected String stageLabel() {
