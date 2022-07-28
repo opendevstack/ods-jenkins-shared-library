b8bcd45240d004b2659fac775b62a94a80ce4001
Martin Marcher <serverhorror@users.noreply.github.com>
2022-07-28 07:24:12 +0000

    Add test cases for copyImage stage

--- test/groovy/vars/OdsComponentStageCopyImageSpec.groovy
+++ test/groovy/vars/OdsComponentStageCopyImageSpec.groovy
@@ -1,24 +1,29 @@
 package vars
 
-
 import org.ods.component.Context
 import org.ods.component.IContext
 import org.ods.services.OpenShiftService
 import org.ods.services.ServiceRegistry
 import org.ods.util.Logger
+import spock.lang.Unroll
 import vars.test_helper.PipelineSpockTestBase
 
 class OdsComponentStageCopyImageSpec extends PipelineSpockTestBase {
 
     private Logger logger = Mock(Logger)
 
+    @Unroll
     def "run copy image command"() {
         given:
+        def sourceImageUrlIncludingRegistry = "${registry}/${repo}/${imageName}:${imageTag}"
         def cfg = [
-            sourceImageUrlIncludingRegistry: 'example.com/repo/image:1f3d1ed76c304893cb709c9b65a64b2cf2e4676b607c6be737c1dddd1cf89065',
+            sourceImageUrlIncludingRegistry: sourceImageUrlIncludingRegistry,
+            verifyTLS                      : verifyTLS,
+            tagIntoTargetEnv               : tagIntoTargetEnv,
         ]
         def ctxCfg = [
             cdProject     : 'project-cd',
+            targetProject : 'project-dev',
             componentId   : 'example-component',
             dockerRegistry: 'internal-registry',
             branch        : 'master',
@@ -26,7 +31,9 @@ class OdsComponentStageCopyImageSpec extends PipelineSpockTestBase {
         ]
         IContext context = new Context(null, ctxCfg, logger)
         OpenShiftService openShiftService = Mock(OpenShiftService.class)
-        openShiftService.findOrCreateImageStream('project-cd', 'example.com/foo/repo/image')
+        openShiftService.findOrCreateImageStream('project-cd', sourceImageUrlIncludingRegistry)
+        openShiftService.getImageReference(*_) >> "${imageName}:${imageTag}"
+
         ServiceRegistry.instance.add(OpenShiftService, openShiftService)
 
         // We allow this method here but do not care about it
@@ -34,19 +41,33 @@ class OdsComponentStageCopyImageSpec extends PipelineSpockTestBase {
 
         when:
         def script = loadScript('vars/odsComponentStageCopyImage.groovy')
+        helper.registerAllowedMethod('readFile', [ Map ]) { "secret-token" }
+//        helper.registerAllowedMethod('sh', [ Map ]) { Map args -> }
         script.call(context, cfg)
 
 
         then:
         printCallStack()
-        assertCallStackContains('(!!! Image successfully imported')
-
-        // FIXME: How do I do this nicely?
-        assertCallStackContains('Resolved source Image data: example.com/repo/image:1f3d1ed76c304893cb709c9b65a64b2cf2e4676b607c6be737c1dddd1cf89065')
-        assertCallStackContains('importing into: docker://internal-registry/project-cd/image:1f3d1ed76c304893cb709c9b65a64b2cf2e4676b607c6be737c1dddd1cf89065')
+        assertCallStackContains("!!! Image successfully imported into internal-registry/project-cd/${imageName}:${imageTag}")
+        assertCallStackContains("Resolved source Image data: ${sourceImageUrlIncludingRegistry}")
+        assertCallStackContains("importing into: docker://internal-registry/project-cd/${imageName}:${imageTag}")
         // FIXME: this should probably verify that the steps.sh is called with the correct string rather than checking the full callstack
-        assertCallStackContains('skopeo copy --src-tls-verify=true                  docker://example.com/repo/image:1f3d1ed76c304893cb709c9b65a64b2cf2e4676b607c6be737c1dddd1cf89065                 --dest-creds openshift:                 docker://internal-registry/project-cd/image:1f3d1ed76c304893cb709c9b65a64b2cf2e4676b607c6be737c1dddd1cf89065                 --dest-tls-verify=true')
+        assertCallStackContains("skopeo copy --src-tls-verify=${expectedVerifyTLS}                  docker://${sourceImageUrlIncludingRegistry}                 --dest-creds openshift:secret-token                 docker://internal-registry/project-cd/image:1f3d1                 --dest-tls-verify=${expectedVerifyTLS}")
+        if (tagIntoTargetEnv) {
+            1 * openShiftService.importImageTagFromProject('project-dev', imageName, 'project-cd', imageTag, imageTag)
+            1 * openShiftService.findOrCreateImageStream('project-dev', imageName)
+        }
 
         assertJobStatusSuccess()
+
+        where:
+        registry      || repo   || imageName || imageTag || verifyTLS || expectedVerifyTLS || tagIntoTargetEnv
+        'example.com' || 'repo' || 'image'   || '1f3d1'  || true      || true              || true
+        'example.com' || 'repo' || 'image'   || '1f3d1'  || true      || true              || false
+        'example.com' || 'repo' || 'image'   || '1f3d1'  || false     || false             || true
+        'example.com' || 'repo' || 'image'   || '1f3d1'  || false     || false             || false
+        'example.com' || 'repo' || 'image'   || '1f3d1'  || null      || true              || true
+        'example.com' || 'repo' || 'image'   || '1f3d1'  || null      || true              || false
+
     }
 }
