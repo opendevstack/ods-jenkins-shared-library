ae257096dbdeb50957150424bcdc688c29bdbc08
Martin Marcher <martin.marcher@boehringer-ingelheim.com>
2022-11-24 11:25:01 +0000

    Detailed exitCode prevents to deploy from scratch

--- src/org/ods/services/OpenShiftService.groovy
+++ src/org/ods/services/OpenShiftService.groovy
@@ -141,7 +141,7 @@ class OpenShiftService {
             diffFlags << '--no-color'
             diffFlags << '--three-way-merge'
             diffFlags << '--normalize-manifests'
-            diffFlags << '--detailed-exitcode'
+            // diffFlags << '--detailed-exitcode'
             steps.sh(
                 script: "HELM_DIFF_IGNORE_UNKNOWN_FLAGS=true helm -n ${project} secrets diff upgrade ${diffFlags.join(' ')} ${release} ./",
                 label: "Show diff explaining what helm upgrade would change for release ${release} in ${project}"
