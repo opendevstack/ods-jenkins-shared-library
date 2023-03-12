63acf23e26e00bf62bf1a3a149d86c3e94cdfa93
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2023-03-12 10:30:06 +0100

    cleanup from Rm into component pipeline

--- vars/odsComponentPipeline.groovy
+++ vars/odsComponentPipeline.groovy
@@ -1,6 +1,10 @@
 import org.ods.component.Pipeline
 import org.ods.util.Logger
 
+import org.ods.services.ServiceRegistry
+import org.ods.util.ClassLoaderCleaner
+import org.ods.util.UnirestConfig
+
 def call(Map config, Closure body) {
     def debug = env.DEBUG
     if (debug != null) {
@@ -12,7 +16,26 @@ def call(Map config, Closure body) {
     def logger = new Logger(this, config.debug)
     def pipeline = new Pipeline(this, logger)
 
-    pipeline.execute(config, body)
+    try {
+        pipeline.execute(config, body)
+    finally {
+        if (!script.env.MULTI_REPO_BUILD) {
+            logger.debug('-- SHUTTING DOWN Component Pipeline (..) --')
+            logger.resetStopwatch()
+            ServiceRegistry.removeInstance()
+            UnirestConfig.shutdown()
+            try {
+                new ClassLoaderCleaner().clean(logger, processId)
+                // use the jenkins INTERNAL cleanupHeap method - attention NOTHING can happen after this method!
+                logger.debug("forceClean via jenkins internals....")
+                Method cleanupHeap = currentBuild.getRawBuild().getExecution().class.getDeclaredMethod("cleanUpHeap")
+                cleanupHeap.setAccessible(true)
+                cleanupHeap.invoke(currentBuild.getRawBuild().getExecution(), null)
+            } catch (Exception e) {
+                logger.debug("cleanupHeap err: ${e}")
+            }            
+        }
+    }
 }
 
 return this
