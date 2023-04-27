d7eee172f96ba07541314c7c59e1d6c7c9071a86
zxBCN Valeriu_Tuguran,Constantin (IT EDP) EXTERNAL <constantin.valeriu_tuguran.ext@boehringer-ingelheim.com>
2023-04-27 15:27:46 +0200

    EDPC-1603 Debug.

--- src/org/ods/orchestration/util/Project.groovy
+++ src/org/ods/orchestration/util/Project.groovy
@@ -422,7 +422,8 @@ class Project {
 
     @NonCPS
     protected Map<String, List> computeWipJiraIssues(Map data) {
-
+        println "computeWipJiraIssues isGxpProject: ${isGxpProject}"
+        println "${data.docs}"
         Map<String, List> result = [:]
         JiraDataItem.COMMON_TYPES_TO_BE_CLOSED.each { type ->
             if (data.containsKey(type)) {
@@ -456,7 +457,7 @@ class Project {
      */
     @NonCPS
     protected Map<String,List> computeWipDocChapterPerDocument(Map data) {
-
+        println "computeWipDocChapterPerDocument method called"
         Map<String, List> result = [:]
         if (isGxpProject()) {
             result = (data[JiraDataItem.TYPE_DOCS] ?: [:])
