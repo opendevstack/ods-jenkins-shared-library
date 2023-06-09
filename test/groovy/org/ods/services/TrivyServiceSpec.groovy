80095d118372a67463a78b2f25c6da0fff5a9096
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2023-06-09 16:17:57 +0200

    add trivyService test

---
+++ test/groovy/org/ods/services/TrivyServiceSpec.groovy
@@ -0,0 +1,41 @@
+package org.ods.services
+
+import org.ods.util.Logger
+import vars.test_helper.PipelineSpockTestBase
+
+class TrivyServiceSpec extends PipelineSpockTestBase {
+
+    def "invoke Trivy cli"() {
+        given:
+        def steps = Spy(util.PipelineSteps)
+        def service = Spy(TrivyService, constructorArgs: [
+            steps,
+            new Logger(steps, false)
+        ])
+
+        when:
+        def result = service.scanViaCli("vuln,config,secret,license", "os,library", "cyclonedx",
+            "trivy-sbom.json", "--debug --timeout=10m")
+
+        then:
+        1 * steps.sh(_) >> {
+            assert it.label == ['Scan via Trivy CLI']
+            assert it.returnStatus == [true]
+            assert it.script.toString().contains('set +e &&')
+            assert it.script.toString().contains('trivy fs')
+            assert it.script.toString().contains('--cache-dir /tmp/.cache')
+            assert it.script.toString().contains('--scanners \'vuln,config,secret,license\'')
+            assert it.script.toString().contains('--vuln-type \'os,library\'')
+            assert it.script.toString().contains('--format \'cyclonedx\'')
+            assert it.script.toString().contains('--output \'trivy-sbom.json\'')
+            assert it.script.toString().contains('--license-full')
+            assert it.script.toString().contains('\'--debug --timeout=10m\'')
+            assert it.script.toString().contains('. &&')
+            assert it.script.toString().contains('set -e')
+
+            return 0
+        }
+        0 == result
+    }
+
+}
