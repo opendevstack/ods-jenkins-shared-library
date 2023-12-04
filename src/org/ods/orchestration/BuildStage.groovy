53e9c1fda8402eb39fb47c6159da6b3dbff4afdd
zxBCN Valeriu_Tuguran,Constantin (IT EDP) EXTERNAL <constantin.valeriu_tuguran.ext@boehringer-ingelheim.com>
2023-12-04 14:49:22 +0100

    Small fix.

--- src/org/ods/orchestration/BuildStage.groovy
+++ src/org/ods/orchestration/BuildStage.groovy
@@ -91,7 +91,7 @@ class BuildStage extends Stage {
         // - this will only apply in case of WIP! - otherwise failfast is configured, and hence
         // the build will have failed beforehand
         def failedRepos = repos?.flatten().findAll { it.data?.failedStage }
-        if (project.hasFailingTests() || failedRepos.size > 0) {
+        if (project.hasFailingTests() || failedRepos?.size > 0) {
             def baseErrMsg = "Failing build as repositories contain errors!" +
                 "\nFailed repositories:\n${sanitizeFailedRepos(failedRepos)}"
 
