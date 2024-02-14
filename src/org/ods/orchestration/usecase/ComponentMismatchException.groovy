aa7bae765a295d0c5ddde3a9a74ddd9de5aa7701
Flavio Campana <flavio.campana.ext@boehringer-ingelheim.com>
2024-02-14 16:38:37 +0100

    fix error

--- src/org/ods/orchestration/usecase/ComponentMismatchException.groovy
+++ src/org/ods/orchestration/usecase/ComponentMismatchException.groovy
@@ -1,14 +1,13 @@
 package org.ods.orchestration.usecase
 
 class ComponentMismatchException  extends IllegalArgumentException {
-    List<String> components
 
-    ComponentMismatchException(String message, List<String> components, Throwable t) {
+    ComponentMismatchException(String message, Throwable t) {
         super(message, t)
         this.components = components
     }
 
-    ComponentMismatchException(String message, List<String> components) {
+    ComponentMismatchException(String message) {
         super(message)
         this.components = components
     }
