253b9aa03829cd5e07e3816a94d3e5973949a396
Hector Rodriguez Cornejo <hector_2.rodriguez@boehringer-ingelheim.com>
2024-03-26 13:45:38 +0100

    Reduce Jira calls (#1083)

---
+++ src/org/ods/orchestration/util/ConcurrentCache.groovy
@@ -0,0 +1,31 @@
+package org.ods.orchestration.util
+
+import com.cloudbees.groovy.cps.NonCPS
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+import java.util.function.Function
+
+class ConcurrentCache<K, V> {
+
+    private final ConcurrentMap<K, V> cache
+    private final Function<? super K, ? extends V> computeIfAbsent
+
+    ConcurrentCache(Function<? super K, ? extends V> computeIfAbsent,
+                    int initialCapacity = 64, float loadFactor = 0.75f, int concurrencyLevel = 1) {
+        this.cache = new ConcurrentHashMap(initialCapacity, loadFactor, concurrencyLevel)
+        this.computeIfAbsent = computeIfAbsent
+    }
+
+    @NonCPS
+    def get(K key) {
+        cache.computeIfAbsent(key, this.computeIfAbsent)
+    }
+
+    @NonCPS
+    @Override
+    String toString() {
+        return cache.toString()
+    }
+
+}
