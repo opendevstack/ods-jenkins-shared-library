fcb9f6bccc62f5eb43379dc797b0b9dfe5db7959
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2024-04-02 18:47:17 +0200

    restore test

--- test/groovy/vars/OdsQuickstarterStageCopyFilesSpec.groovy
+++ test/groovy/vars/OdsQuickstarterStageCopyFilesSpec.groovy
@@ -2,22 +2,18 @@ package vars
 
 import org.ods.quickstarter.Context
 import org.ods.quickstarter.IContext
-import org.ods.util.Logger
 import vars.test_helper.PipelineSpockTestBase
-import util.PipelineSteps
 import spock.lang.*
 
 class OdsQuickstarterStageCopyFilesSpec extends PipelineSpockTestBase {
-
-  private Logger logger = new Logger (new PipelineSteps(), true)
-  
+ 
   def "run successfully"() {
     given:
     def config = [
         sourceDir: 'be-golang-plain',
         targetDir: 'out'
     ]
-    IContext context = new Context(config, logger, null)
+    IContext context = new Context(config)
 
     when:
     def script = loadScript('vars/odsQuickstarterStageCopyFiles.groovy')
