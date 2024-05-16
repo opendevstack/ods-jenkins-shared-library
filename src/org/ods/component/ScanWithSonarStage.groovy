752e69f59cbb26225d4d3e5b0da1bd99eb725d7a
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2024-05-16 15:55:15 +0200

    codeNarc fixes

--- src/org/ods/component/ScanWithSonarStage.groovy
+++ src/org/ods/component/ScanWithSonarStage.groovy
@@ -115,7 +115,7 @@ class ScanWithSonarStage extends Stage {
         def report = generateTempFileFromReport("artifacts/" + context.getBuildArtifactURIs().get('SCRR-MD'))
         URI reportUriNexus = generateAndArchiveReportInNexus(report,
             context.sonarQubeNexusRepository ? context.sonarQubeNexusRepository : DEFAULT_NEXUS_REPOSITORY)
-        createBitbucketCodeInsightReport(qualityGateResult, reportUriNexus.toString(), sonarProjectKey, 
+        createBitbucketCodeInsightReport(qualityGateResult, reportUriNexus.toString(), sonarProjectKey,
             context.sonarQubeEdition, context.gitBranch)
     }
 
@@ -254,18 +254,18 @@ class ScanWithSonarStage extends Stage {
         }
     }
 
-    private createBitbucketCodeInsightReport(String qualityGateResult, String nexusUrlReport, String sonarProjectKey, String edition, String branch) {
-        
+    private createBitbucketCodeInsightReport(
+        String qualityGateResult, String nexusUrlReport,String sonarProjectKey, String edition, String branch) {
         String sorQubeScanUrl = ""
         String title = "SonarQube"
         String details = "Please visit the following links to review the SonarQube report:"
         String result = qualityGateResult == "OK" ? "PASS" : "FAIL"
 
         if (edition != 'community') {
-                sorQubeScanUrl = sonarQube.getSonarQubeHostUrl() + "/dashboard?id=${sonarProjectKey}&branch=${branch}"
-            } else {
-                sorQubeScanUrl = sonarQube.getSonarQubeHostUrl() + "/dashboard?id=${sonarProjectKey}"
-            }
+            sorQubeScanUrl = sonarQube.getSonarQubeHostUrl() + "/dashboard?id=${sonarProjectKey}&branch=${branch}"
+        } else {
+            sorQubeScanUrl = sonarQube.getSonarQubeHostUrl() + "/dashboard?id=${sonarProjectKey}"
+        }
 
         def data = [
             key: BITBUCKET_SONARQUBE_REPORT_KEY,
