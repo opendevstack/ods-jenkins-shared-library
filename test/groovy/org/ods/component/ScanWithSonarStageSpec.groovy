e06f58b1be15d59f0f96c122a82d9b5530ba7873
brais <26645694+braisvq1996@users.noreply.github.com>
2024-05-17 10:17:19 +0200

    Included the complete Requirement description to Risk Assessment Table (#1098)
    * EDPC-2185::Risk Assesment Table.
    
    * EDPC-2185::Risk Assesment Table.
    
    * EDPC-2185::added CHANGELOG.
    
    * EDPC-2185::Logic for TST.
    
    * EDPC-2185::Refactoring the Risk Assesment table requirements.
    
    * EDPC-2185::Refactoring the Risk Assesment table requirements.
    
    * EDPC-2185::Refactoring the Risk Assesment table requirements.
    
    * EDPC-2185::Refactoring the Risk Assesment table requirements.
    
    * EDPC-2185::Refactoring the Risk Assesment table requirements.
    
    * EDPC-2185::Refactoring the Risk Assesment table requirements.
    
    * EDPC-2185::Refactor for CHANGELOG.md

--- test/groovy/org/ods/component/ScanWithSonarStageSpec.groovy
+++ test/groovy/org/ods/component/ScanWithSonarStageSpec.groovy
@@ -95,6 +95,8 @@ class ScanWithSonarStageSpec extends PipelineSpockTestBase {
         given:
         def tempFolderPath = tempFolder.getRoot().absolutePath
         def stage = createStage(tempFolderPath)
+        def edition = "enterprise"
+        def branch = "develop"
         def data = [
             key: ScanWithSonarStage.BITBUCKET_SONARQUBE_REPORT_KEY,
             title: "SonarQube",
@@ -103,7 +105,7 @@ class ScanWithSonarStageSpec extends PipelineSpockTestBase {
                 [
                     title: "Report",
                     text: "Result in SonarQube",
-                    link: "https://sonarqube.example.com/dashboard?id=prj1-component1"
+                    link: "https://sonarqube.example.com/dashboard?id=prj1-component1&branch=develop"
                 ],
                 [
                     title: "Report",
@@ -116,7 +118,7 @@ class ScanWithSonarStageSpec extends PipelineSpockTestBase {
         ]
 
         when:
-        stage.createBitbucketCodeInsightReport("OK", "http://nexus", "prj1-component1")
+        stage.createBitbucketCodeInsightReport("OK", "http://nexus", "prj1-component1", "enterprise", "develop")
 
         then:
         1 * stage.bitbucket.createCodeInsightReport(data, stage.context.repoName, stage.context.gitCommit)
@@ -126,6 +128,8 @@ class ScanWithSonarStageSpec extends PipelineSpockTestBase {
         given:
         def tempFolderPath = tempFolder.getRoot().absolutePath
         def stage = createStage(tempFolderPath)
+        def edition = "enterprise"
+        def branch = "develop"
         def data = [
             key: ScanWithSonarStage.BITBUCKET_SONARQUBE_REPORT_KEY,
             title: "SonarQube",
@@ -134,7 +138,7 @@ class ScanWithSonarStageSpec extends PipelineSpockTestBase {
                 [
                     title: "Report",
                     text: "Result in SonarQube",
-                    link: "https://sonarqube.example.com/dashboard?id=prj1-component1"
+                    link: "https://sonarqube.example.com/dashboard?id=prj1-component1&branch=develop"
                 ],
                 [
                     title: "Report",
@@ -147,7 +151,7 @@ class ScanWithSonarStageSpec extends PipelineSpockTestBase {
         ]
 
         when:
-        stage.createBitbucketCodeInsightReport("ERROR", "http://nexus", "prj1-component1")
+        stage.createBitbucketCodeInsightReport("ERROR", "http://nexus", "prj1-component1", "enterprise", "develop")
 
         then:
         1 * stage.bitbucket.createCodeInsightReport(data, stage.context.repoName, stage.context.gitCommit)
