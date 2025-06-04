cb4a9a0e2f8efc8f55e652f9f079c0c3010f40e1
Hector Rodriguez Cornejo <hector_2.rodriguez@boehringer-ingelheim.com>
2025-06-04 10:31:27 +0200

    Avoid check PROD environment if repos are not included in release (#1217)

--- src/org/ods/orchestration/DeployStage.groovy
+++ src/org/ods/orchestration/DeployStage.groovy
@@ -88,7 +88,9 @@ class DeployStage extends Stage {
                 def targetEnvironment = project.buildParams.targetEnvironment
                 def targetProject = project.targetProject
                 def installableRepos = this.project.repositories.findAll { repo ->
-                    if (repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_INFRA) {
+                    // We only manage the installable repositories in OpenShift if they are included in the release
+                    if (repo.include
+                        && repo.type?.toLowerCase() != MROPipelineUtil.PipelineConfig.REPO_TYPE_ODS_INFRA) {
                         MROPipelineUtil.PipelineConfig.INSTALLABLE_REPO_TYPES.contains(repo.type)
                     }
                 }
