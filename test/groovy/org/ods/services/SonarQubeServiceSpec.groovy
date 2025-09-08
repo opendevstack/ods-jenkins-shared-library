7e01400f73e94631bc34fc6313bd7920848043a6
Vazquez,Brais (IT EDP) <brais.vazquez@boehringer-ingelheim.com>
2025-09-08 11:18:08 +0000

    add new test to service

--- test/groovy/org/ods/services/SonarQubeServiceSpec.groovy
+++ test/groovy/org/ods/services/SonarQubeServiceSpec.groovy
@@ -102,4 +102,87 @@ class SonarQubeServiceSpec extends PipelineSpockTestBase {
         1 * steps.sh(label: 'Set Java 17 for SonarQube scan', script: "source use-j17.sh")
         1 * steps.sh(label: 'Run SonarQube scan', script: { it.contains("/opt/sonar-scanner/bin/sonar-scanner") && it.contains("-Dsonar.projectKey=key") && it.contains("-Dsonar.projectName=name") && it.contains("-Dsonar.exclusions=test/**") })
     }
+
+    def "generateCNESReport calls script.sh with correct params"() {
+        given:
+        def steps = GroovyMock(util.PipelineSteps)
+        steps.withSonarQubeEnv(_, _) >> { String env, Closure block -> block() }
+        steps.sh(_) >> null
+        def logger = Mock(org.ods.util.ILogger)
+        logger.shellScriptDebugFlag >> "set -x"
+        def service = new SonarQubeService(steps, logger, "env")
+
+        when:
+        service.generateCNESReport("projectKey", "author", "branch", "developer", "token")
+
+        then:
+        1 * steps.sh(label: 'Generate CNES Report', script: { it.contains("java -jar /usr/local/cnes/cnesreport.jar") && it.contains("-s") && it.contains("-t token") && it.contains("-p projectKey") && it.contains("-a author") && it.contains("-b branch") })
+    }
+
+    def "getQualityGateJSON calls script.sh with correct params"() {
+        given:
+        def steps = GroovyMock(util.PipelineSteps)
+        steps.withSonarQubeEnv(_, _) >> { String env, Closure block -> block() }
+        steps.sh(_ as Map) >> "{}"
+        def logger = Mock(org.ods.util.ILogger)
+        def service = new SonarQubeService(steps, logger, "env")
+
+        when:
+        service.getQualityGateJSON("projectKey", "developer", "branch", "prKey", "token")
+
+        then:
+        1 * steps.sh({ args ->
+            args.label == 'Get status of quality gate' &&
+            args.script.contains("curl") &&
+            args.script.contains("--data-urlencode projectKey=projectKey") &&
+            args.script.contains("--data-urlencode pullRequest=prKey") &&
+            args.script.contains("-u token:") &&
+            args.returnStdout == true
+        })
+    }
+
+    def "getComputeEngineTaskJSON calls script.sh with correct params"() {
+        given:
+        def steps = GroovyMock(util.PipelineSteps)
+        steps.withSonarQubeEnv(_, _) >> { String env, Closure block -> block() }
+        steps.sh(_ as Map) >> "{}"
+        def logger = Mock(org.ods.util.ILogger)
+        def service = new SonarQubeService(steps, logger, "env")
+
+        when:
+        service.getComputeEngineTaskJSON("taskid", "token")
+
+        then:
+        1 * steps.sh({ args ->
+            args.label == 'Get status of compute engine task' &&
+            args.script.contains("curl") &&
+            args.script.contains("api/ce/task?id=taskid") &&
+            args.script.contains("-u token:") &&
+            args.returnStdout == true
+        })
+    }
+
+    def "generateAndStoreSonarQubeToken returns empty string if secret exists"() {
+        given:
+        def steps = GroovyMock(util.PipelineSteps)
+        steps.sh(_) >> "some-secret"
+        def logger = Mock(org.ods.util.ILogger)
+        logger.info(_) >> null
+        def service = new SonarQubeService(steps, logger, "env")
+
+        expect:
+        service.generateAndStoreSonarQubeToken("credId", "namespace", "secretName") == ""
+    }
+
+    def "getScannerBinary returns tool path if which fails"() {
+        given:
+        def steps = GroovyMock(util.PipelineSteps)
+        steps.sh(_) >> 1
+        steps.tool('SonarScanner') >> '/opt/sonar-scanner'
+        def logger = Mock(org.ods.util.ILogger)
+        def service = new SonarQubeService(steps, logger, "env")
+
+        expect:
+        service.getScannerBinary() == "/opt/sonar-scanner/bin/sonar-scanner"
+    }
 }
