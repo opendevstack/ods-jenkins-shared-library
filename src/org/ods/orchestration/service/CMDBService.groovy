c41ba8c70971cab64ec4d831a58aac133ca430df
Etmajer,DI,Martin (IT CTO) BIG-AT-V <martin.etmajer@boehringer-ingelheim.com>
2026-01-07 19:50:48 +0100

    Add parsing of ci service

--- src/org/ods/orchestration/service/CMDBService.groovy
+++ src/org/ods/orchestration/service/CMDBService.groovy
@@ -52,7 +52,7 @@ class CMDBService {
         }
     }
 
-    private static final int MAX_DEPTH = 3
+    private static final int MAX_DEPTH = 5
     private static final List SYSPARM_CI_FIELDS = ["sys_id", "sys_class_name", "name", "u_name_business_friendly", "application_type", "install_type", "short_description", "owned_by.email", "managed_by.email", "u_gxp_relevant", "u_gxp_criticality", "u_slc_documents_location_items_and_con", "u_system_design_specification_link", "u_validation_determination_reference", "u_gamp_category", "service_classification"]
     private static final int SYSPARM_CI_RELATIONS_LIMIT = 1000
     private static final List SYSPARM_CMDB_CI_FIELDS = ["sys_class_name"]
@@ -191,10 +191,10 @@ class CMDBService {
                     logger.debug "!!! Depth: ${depth}, RelationName: ${relationType.name}, SysClassName: ${childSysClass.sys_class_name}, ChildCIName: ${childNode.name}"
                   
                     //if (!(childSysClass.sys_class_name ==~ /(?i)exception|software|branch|leaf|server|database|business_app|service/)) {
-                    if (childSysClass.sys_class_name ==~ /(?i)u_cmdb_ci_interface|cmdb_ci_db_catalog|cmdb_ci_database/) {
+                    if (childSysClass.sys_class_name ==~ /(?i)u_cmdb_ci_interface|cmdb_ci_db_catalog|cmdb_ci_database|cmdb_ci_service/) {
                         loadRelationalCisData(childNode, ciRelationsLookupStrategy, relationSanitizerStrategy, parentNodeIds, depth + 1, maxDepth)
                     } else {
-                        logger.debug "No further relationships looked up for class ${childSysClass.sys_class_name}\n"
+                        logger.debug "No further relationships look-up configured for child class ${childSysClass.sys_class_name} and child name ${childNode.name}\n"
                     }
                 }
             // }
