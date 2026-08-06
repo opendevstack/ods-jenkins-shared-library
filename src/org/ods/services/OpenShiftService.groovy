e9e535c4e5c87d2ca76abcdc10d0c68114904a43
zxBCN Martin_Castro,Jose (IT EDP) EXTERNAL <jose.martin_castro.ext@boehringer-ingelheim.com>
2026-08-06 14:14:17 +0200

    Refactor loginToExternalCluster to use single quotes for string interpolation and enhance environment variable handling

--- src/org/ods/services/OpenShiftService.groovy
+++ src/org/ods/services/OpenShiftService.groovy
@@ -44,7 +44,7 @@ class OpenShiftService {
         ]
         steps.withEnv(envVars) {
             steps.sh(
-                script: 'oc login "${OPENSHIFT_API_URL}" --token="${OPENSHIFT_API_TOKEN}" >& /dev/null',
+                script: "oc login '${OPENSHIFT_API_URL}' --token='${OPENSHIFT_API_TOKEN}' >& /dev/null",
                 label: "login to external cluster (${apiUrl})"
             )
         }
@@ -1515,15 +1515,23 @@ class OpenShiftService {
             label: 'Read OpenShift service-account token'
         ).toString().trim()
 
-        def success = steps.sh(
-            script: '''
-                \${logger.shellScriptDebugFlag}
-                oc login "\${kubeUrl}" --insecure-skip-tls-verify=true \
-                --token="\${token}" &> /dev/null
-            ''',
-            returnStatus: true,
-            label: 'Check if OCP session exists'
-        ) == 0
+        def success = false
+        List<String> envVars = [
+            "K8S_URL=${kubeUrl}".toString(),
+            "K8S_TOKEN=${token}".toString()
+        ]
+
+        steps.withEnv(envVars) {
+            success = steps.sh(
+                script: """
+                    ${logger.shellScriptDebugFlag}
+                    oc login '${K8S_URL}' --insecure-skip-tls-verify=true \
+                    --token='${K8S_TOKEN}' &> /dev/null
+                """,
+                returnStatus: true,
+                label: 'Check if OCP session exists'
+            ) == 0
+        }
 
         if (!success) {
             throw new RuntimeException(
