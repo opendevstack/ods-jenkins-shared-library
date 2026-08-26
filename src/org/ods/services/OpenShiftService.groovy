4a732084dfafaf7ec6aa3c558c724e3911be5e8d
zxBCN Valeriu_Tuguran,Constantin (IT EDP) EXTERNAL <constantin.valeriu_tuguran.ext@boehringer-ingelheim.com>
2026-08-26 16:44:26 +0200

    Debug.

--- src/org/ods/services/OpenShiftService.groovy
+++ src/org/ods/services/OpenShiftService.groovy
@@ -1527,8 +1527,9 @@ class OpenShiftService {
         steps.withEnv(envVars) {
             success = steps.sh(
                 script: '''
-                    ${logger.shellScriptDebugFlag}
-                    oc login "${K8S_URL}" --insecure-skip-tls-verify=true \
+                    ${logger.shellScriptDebugFlag} \
+                    oc login "${K8S_URL}" \
+                    --insecure-skip-tls-verify=true \
                     --token="${K8S_TOKEN}" &> /dev/null
                 ''',
                 returnStatus: true,
